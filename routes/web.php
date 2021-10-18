4ed23f620f07d7bba3c4c6ea88824d3b0f815375
Foisal Ahmed <43063960+FoishaalAhmed@users.noreply.github.com>
2021-10-18 20:09:14 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,16 +16,22 @@ use Illuminate\Support\Facades\Route;
 // Task 1: point the main "/" URL to the HomeController method "index"
 // Put one code line here below
 
+Route::get('/', [HomeController::class, 'index']);
+
 
 // Task 2: point the GET URL "/user/[name]" to the UserController method "show"
 // It doesn't use Route Model Binding, it expects $name as a parameter
 // Put one code line here below
+
+Route::get('/user/{name}', [UserController::class, 'show']);
 
 
 // Task 3: point the GET URL "/about" to the view
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
+
+Route::view('/about', 'pages/about')->name('about');
 
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
@@ -36,11 +42,23 @@ use Illuminate\Support\Facades\Route;
 // Assign middleware "auth"
 // Put one Route Group code line here below
 
+Route::group(['middelware' => 'auth'], function () {
+    
+});
+
+
     // Tasks inside that Authenticated group:
 
     // Task 6: /app group within a group
     // Add another group for routes with prefix "app"
     // Put one Route Group code line here below
+
+        Route::group(['middelware' => 'auth'], function () {
+            Route::group(['prefix' => '/app'], function () {
+                Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+                Route::resource('/tasks', DashboardController::class);
+            });
+        });
 
         // Tasks inside that /app group:
 
@@ -64,6 +82,18 @@ use Illuminate\Support\Facades\Route;
 
 
         // Tasks inside that /app group:
+
+        Route::group(['middelware' => 'auth'], function () {
+            Route::group(['prefix' => '/app'], function () {
+                Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+                Route::resource('/tasks', DashboardController::class);
+                
+                Route::group(['prefix' => '/admin', 'middleware' => ['is_admin']], function () {
+                     Route::get('/dashboard', [Admin/DashboardController::class, 'index'])->name('dashboard');
+                     Route::get('/stats', [Admin/StatsController::class, 'index']);
+                });
+            });
+        });
 
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
