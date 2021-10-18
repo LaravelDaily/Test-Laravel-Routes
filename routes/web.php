481c11c09ce1d5b4356fbc0540a108a449b86457
Vili Taka-Anttila <vili.taka-anttila@lyyti.com>
2021-10-18 16:21:10 +0300

    Fix naming

--- routes/web.php
+++ routes/web.php
@@ -67,10 +67,10 @@ Route::middleware('auth')->group(function () {
         // Tasks inside that /app group:
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::get('/dashboard', Admin\DashboardController::class);
+        Route::get('/dashboard', AdminDashboardController::class);
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
-        Route::get('/stats', Admin\StatsController::class);
+        Route::get('/stats', AdminStatsController::class);
     });
     // End of the /admin Route Group
 });
