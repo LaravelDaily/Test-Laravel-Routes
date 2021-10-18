4c9907b48ef737032331c6ce8d024ffc07f4b4ac
veerman72 <64311955+veerman72@users.noreply.github.com>
2021-10-18 10:38:55 +0200

    routes test with proper use of Admin/DashboardController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use App\Http\Controllers\Admin\StatsController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TaskController;
@@ -78,7 +79,7 @@ Route::middleware(['auth'])->group(function () {
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::get('dashboard', DashboardController::class);
+        Route::get('dashboard', AdminDashboardController::class);
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
