511a2ff194017bfb70d5ea9fee9af122cbe2fa4e
Jared Lewis <jslewis90@outlook.com>
2021-10-18 02:25:59 -0400

    Task 3

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/user/{user:name}', [\App\Http\Controllers\UserController::class, 's
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-
+Route::view('/about', 'pages.about')->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
