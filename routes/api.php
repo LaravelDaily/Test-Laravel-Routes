93ec9986063a068290a564ccac4418f9b7d8650b
Ibrahim Lasisi <ilasisi90@gmail.com>
2021-10-18 07:54:57 +0100

    Complete all tasks

--- routes/api.php
+++ routes/api.php
@@ -20,8 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::group(['middleware' => 'auth:sanctum'], function() {
     // Task 12: Manage tasks with endpoint /api/v1/tasks/*****.
-    // Keep in mind that prefix should be /api/v1.
-    // Add ONE line to assign 5 resource routes to TaskController
-    // Put one code line here below
+    Route::apiResource('api/v1/tasks', App\Http\Controllers\Api\V1\TaskController::class);
 
 });
