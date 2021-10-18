5fcd5d5c28cf7cc569902ede60ecba4812c5e8f1
Michael Tea <michael@fashionvalet.com>
2021-10-18 17:32:49 +0800

    completed route tests

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function(Request $request) {
     return $request->user();
 });
 
@@ -23,5 +23,7 @@ Route::group(['middleware' => 'auth:sanctum'], function() {
     // Keep in mind that prefix should be /api/v1.
     // Add ONE line to assign 5 resource routes to TaskController
     // Put one code line here below
-
+    Route::group(['prefix' => 'v1'], function() {
+        Route::resource('tasks', \App\Http\Controllers\Api\V1\TaskController::class);
+    });
 });
