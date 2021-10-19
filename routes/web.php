2fd4297d36d651df2fcdf5ccb52a83849f2381ab
Vili Taka-Anttila <vili.taka-anttila@lyyti.com>
2021-10-19 08:37:20 +0300

    Update url naming

--- routes/web.php
+++ routes/web.php
@@ -65,11 +65,11 @@ Route::middleware('auth')->group(function () {
     // Add a group for routes with URL prefix "admin"
     // Assign middleware called "is_admin" to them
     // Put one Route Group code line here below
-    Route::middleware('is_admin')->prefix('/admin')->group(function () {
+    Route::middleware('is_admin')->prefix('admin')->group(function () {
         // Tasks inside that /app group:
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::get('/dashboard', AdminDashboardController::class);
+        Route::get('dashboard', AdminDashboardController::class);
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
         Route::get('stats', AdminStatsController::class);
