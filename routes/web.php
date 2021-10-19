9e7334e8352663fec1062d913ed31df814fe3a24
Danya Shakhov <danilshahov@gmai.com>
2021-10-19 14:12:13 +0300

    last5

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use App\Http\Controllers\Admin\StatsController;
-use App\Http\Controllers\Api\V1\TaskController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -89,7 +90,7 @@ Route::get('/log-in', fn() => redirect('login'));
     // End of the /admin Route Group
     Route::group(['middleware'=>'auth'], function(){
         Route::group(['prefix'=>'admin', 'middleware'=>'is_admin'], function(){
-            Route::get('dashboard', DashboardController::class);
+            Route::get('dashboard', AdminDashboardController::class);
             Route::get('stats', StatsController::class);
         });
     });
