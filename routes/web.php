b8de9741f0ff9753d3bc286570796c4ba9027c64
Nehal Patel <patelnehal4u@gmail.com>
2021-10-20 21:33:22 +0530

    Laravel route test finished

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,13 @@ use Illuminate\Support\Facades\Route;
 
 // Task 1: point the main "/" URL to the HomeController method "index"
 // Put one code line here below
-Route::get('/', [\App\Http\Controllers\HomeController::class, "index"]);
+Route::get('/', [App\Http\Controllers\HomeController::class, "index"]);
 
 
 // Task 2: point the GET URL "/user/[name]" to the UserController method "show"
 // It doesn't use Route Model Binding, it expects $name as a parameter
 // Put one code line here below
-Route::get('/user/{name}', [\App\Http\Controllers\UserController::class, 'show']);
+Route::get('/user/{name}', [App\Http\Controllers\UserController::class, 'show']);
 
 
 // Task 3: point the GET URL "/about" to the view
@@ -56,13 +56,13 @@ Route::middleware(['auth'])->group(function () {
         // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
         // Assign the route name "dashboard"
         // Put one Route Group code line here below
-        Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class])->name('dashboard');
+        Route::get('/dashboard', [App\Http\Controllers\DashboardController::class])->name('dashboard');
 
 
         // Task 8: Manage tasks with URL /app/tasks/***.
         // Add ONE line to assign 7 resource routes to TaskController
         // Put one code line here below
-        Route::resource('tasks', \App\Http\Controllers\TaskController::class);
+        Route::resource('tasks', App\Http\Controllers\TaskController::class);
 
         // End of the /app Route Group
     });
@@ -80,12 +80,12 @@ Route::middleware(['auth'])->group(function () {
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::post('/dashboard', [\App\Http\Controllers\Admin\DashboardController::class]);
+        Route::get('/dashboard', App\Http\Controllers\Admin\DashboardController::class);
 
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
-        Route::post('/stats', [\App\Http\Controllers\Admin\StatsController::class]);
+        Route::get('/stats', App\Http\Controllers\Admin\StatsController::class);
 
 
         // End of the /admin Route Group
