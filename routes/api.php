3afb93a78cd0fe4be40e0c0c2fa528db9b95f940
Suraj Paudel <paudel.suraj76@gmail.com>
2021-10-23 20:37:43 +0545

    API ROUTE UPDATED SUCCESSFULLY.

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\TaskController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -14,16 +13,13 @@ use App\Http\Controllers\Api\V1\TaskController;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+ return $request->user();
 });
-
-    // Task 12: Manage tasks with endpoint /api/v1/tasks/*****.
-    // Keep in mind that prefix should be /api/v1.
-    // Add ONE line to assign 5 resource routes to TaskController
-    // Put one code line here below
-   Route::apiResource('/v1/tasks', TaskController::class)->except('create', 'edit');
-
-});
+// Task 12: Manage tasks with endpoint /api/v1/tasks/*****.
+// Keep in mind that prefix should be /api/v1.
+// Add ONE line to assign 5 resource routes to TaskController
+// Put one code line here below
+Route::apiResource('/v1/tasks', TaskController::class)->except('create', 'edit');
