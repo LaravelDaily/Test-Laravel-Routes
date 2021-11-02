974bedd3d723ff1bb36d8f49dbcd17bfa794c69b
mkarimi <mojtaba.karimi@solve-x.net>
2021-11-03 00:52:28 +0330

    Tasks  test 4

--- routes/web.php
+++ routes/web.php
@@ -74,7 +74,7 @@ Route::middleware('auth')->group(function () {
     // Add a group for routes with URL prefix "admin"
     // Assign middleware called "is_admin" to them
     // Put one Route Group code line here below
-    Route::group(['prefix' => 'admin'], function () {
+    Route::prefix( 'admin')->middleware('is_admin')->group( function () {
 
     // Tasks inside that /admin group:
 
@@ -89,7 +89,7 @@ Route::middleware('auth')->group(function () {
         Route::get('stats' , App\Http\Controllers\Admin\StatsController::class);
 
     // End of the /admin Route Group
-})->middleware('is_admin');
+});
 // End of the main Authenticated Route Group
 });
 // One more task is in routes/api.php
