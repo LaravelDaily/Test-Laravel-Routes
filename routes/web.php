9673aaa91ecfce98bd2ee11f31885a8e15959917
Md. Abir Chowdhury <40237786+higher18@users.noreply.github.com>
2021-11-05 16:38:00 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 // Put one code line here below
 Route::get("/",function(){
     HomeController("index");
-    );
+});
 
 // Task 2: point the GET URL "/user/[name]" to the UserController method "show"
 // It doesn't use Route Model Binding, it expects $name as a parameter
