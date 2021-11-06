dfaf24857b0fc76fcba05a604170e18eae4473bc
Md. Abir Chowdhury <40237786+higher18@users.noreply.github.com>
2021-11-06 10:03:30 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,8 @@ Route::get('/user/{name}', [UserController::class, 'show']);
 // Also, assign the route name "about"
 // Put one code line here below
  Route::get('/about', function () {
-      return view('pages/about');  
-    })->name('about');
+        return view('pages/about');
+    })->name('About');
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
  Route::get('log-in',function(){
