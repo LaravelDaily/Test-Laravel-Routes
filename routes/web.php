6647b718faef8ff0723b79ab28095878e95e3982
Md. Abir Chowdhury <40237786+higher18@users.noreply.github.com>
2021-11-06 09:50:19 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -28,11 +28,14 @@ Route::get('/user/{name}', [UserController::class, 'show']);
 // Also, assign the route name "about"
 // Put one code line here below
  Route::get('/about', function () {
-        
+      return view('pages/about');  
     })->name('about');
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
-
+ Route:get('log-in',function(){
+       $url = route('login');
+        return url;
+    });
 
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
