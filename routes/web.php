825e028aa695d2fe380fe5c643cbbeb9f89f3b53
PhyoHtetAungSCM <scm.phyohtetaung@gmail.com>
2021-11-06 12:51:15 +0630

    test laravel routes - 4th attempt

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use App\Http\Controllers\Admin\StatsController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -72,7 +73,7 @@ Route::group(['middleware' => 'auth'], function () {
 
 // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
 // Put one code line here below
-        Route::get('/dashboard', DashboardController::class);
+        Route::get('/dashboard', AdminDashboardController::class);
 
 // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
 // Put one code line here below
