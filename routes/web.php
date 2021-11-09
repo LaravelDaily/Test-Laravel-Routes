ab1bd77726144ab4dd0aa6dcf29d8ac1d9aa3424
MartinZ96 <martynasss16@gmail.com>
2021-11-09 12:00:04 +0200

    Tasks 3-5

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\controllers\UserController;
 // Task 1: point the main "/" URL to the HomeController method "index"
 // Put one code line here below
 
-Route::get('/home', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
 
 
 // Task 2: point the GET URL "/user/[name]" to the UserController method "show"
@@ -32,15 +32,18 @@ Route::get('/user/{name}', [UserController::class, 'show']);
 // Also, assign the route name "about"
 // Put one code line here below
 
-
+Route::view('/about', 'pages.about')->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
 
+Route::redirect('log-in', 'login');
 
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
 // Put one Route Group code line here below
+
+Route::group(['middleware' => 'auth'], function () {});
 
     // Tasks inside that Authenticated group:
 
