dc664e916971de4a6c47a7d9b756b137ca359d07
roshan00017 <57831675+roshan00017@users.noreply.github.com>
2021-11-27 16:58:31 +0545

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
+/*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
