665b2e41e33f83efd09ddc1de1a75408e902296c
Sergey Zagarov <m555pa55@google.com>
2021-11-27 16:44:21 +0600

    fix api error

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware' => 'auth:sanctum', 'prefix' => '/api/v1'], function() {
+Route::group(['middleware' => 'auth:sanctum', 'prefix' => 'v1'], function() {
     // Task 12: Manage tasks with endpoint /api/v1/tasks/*****.
     // Keep in mind that prefix should be /api/v1.
     // Add ONE line to assign 5 resource routes to TaskController
