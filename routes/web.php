73c8621b22dd51f5885920d29ee707200adcef36
Muhammad Nur Ramadhan Alam <50946665+anemadhon@users.noreply.github.com>
2021-12-03 09:26:40 +0700

    Update web.php
    task 11 - route

--- routes/web.php
+++ routes/web.php
@@ -89,7 +89,7 @@ Route::group([
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
-
+        Route::get('/stats', \App\Http\Controllers\Admin\StatsController::class);
     });
     // End of the /admin Route Group
 });
