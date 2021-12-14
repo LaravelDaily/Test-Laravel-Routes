e5e959073c78abb52cf2109c034a48b24da2e7b0
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2021-12-14 21:00:43 +0700

    fixing undefine route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,5 +23,5 @@ Route::group(['middleware' => 'auth:sanctum', 'prefix' => 'v1'], function () {
     // Keep in mind that prefix should be /api/v1.
     // Add ONE line to assign 5 resource routes to TaskController
     // Put one code line here below
-    Route::apiResource('tasks', TaskController::class);
+    Route::apiResource('tasks', App\Http\Controllers\Api\V1\TaskController::class);
 });
