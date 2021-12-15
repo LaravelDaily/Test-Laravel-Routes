b0e3d1d3d7bef109ecf73de2aa0ada6ac14cc397
Cameron Howell <cwhowell@mail.roanoke.edu>
2021-12-15 02:46:04 -0500

    2

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Admin\StatsController;
-use App\Http\Controllers\Api\V1\TaskController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
