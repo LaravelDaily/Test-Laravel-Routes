ba62fa9c617d9bd7b9ed315e0a8fb099fcbbc4db
Nerijus <nerijus.bukartas@gmail.com>
2022-01-01 21:51:04 +0200

    pest it

--- tests/Feature/RoutesTest.php
+++ tests/Feature/RoutesTest.php
@@ -1,124 +1,107 @@
 <?php
 
-namespace Tests\Feature;
-
-use App\Models\Task;
 use App\Models\User;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Models\Task;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
 
-class RoutesTest extends TestCase
-{
-    use RefreshDatabase;
+uses(RefreshDatabase::class);
 
-    public function test_home_screen_shows_welcome()
-    {
-        $response = $this->get('/');
+it('shows welcome on home screen', function () {
+    $response = $this->get('/');
 
-        $response->assertViewIs('welcome');
-        $response->assertViewHas('pageTitle', 'Homepage');
-    }
+    $response->assertViewIs('welcome');
+    $response->assertViewHas('pageTitle', 'Homepage');
+});
 
-    public function test_user_page_existing_user_found()
-    {
-        $user = User::factory()->create();
-        $response = $this->get('/user/' . $user->name);
+it('existing users page found', function () {
+    $user = User::factory()->create();
+    $response = $this->get('/user/' . $user->name);
 
-        $response->assertOk();
-        $response->assertViewIs('users.show');
-    }
+    $response->assertOk();
+    $response->assertViewIs('users.show');
+});
 
-    public function test_user_page_nonexisting_user_not_found()
-    {
-        $response = $this->get('/user/sometotallynonexistinguser');
-        $response->assertViewIs('users.notfound');
-    }
+it('nonexisting users page not found', function () {
+    $response = $this->get('/user/sometotallynonexistinguser');
+    $response->assertViewIs('users.notfound');
+});
 
-    public function test_about_page_is_loaded()
-    {
-        $response = $this->get('/about');
+it('loads about page', function () {
+    $response = $this->get('/about');
 
-        $response->assertViewIs('pages.about');
-    }
+    $response->assertViewIs('pages.about');
+});
 
-    public function test_auth_middleware_is_working()
-    {
-        $response = $this->get('/app/dashboard');
-        $response->assertRedirect('/login');
+it('checks if auth middleware works', function () {
+    $response = $this->get('/app/dashboard');
+    $response->assertRedirect('/login');
 
-        $response = $this->get('/app/tasks');
-        $response->assertRedirect('/login');
-    }
+    $response = $this->get('/app/tasks');
+    $response->assertRedirect('/login');
+});
 
+it('checks if tasks crud is working', function () {
+    $user = User::factory()->create();
 
-    public function test_task_crud_is_working()
-    {
-        $user = User::factory()->create();
+    $response = $this->actingAs($user)->get('/app/tasks');
+    $response->assertOk();
 
-        $response = $this->actingAs($user)->get('/app/tasks');
-        $response->assertOk();
+    $response = $this->actingAs($user)->get('/app/tasks/create');
+    $response->assertOk();
 
-        $response = $this->actingAs($user)->get('/app/tasks/create');
-        $response->assertOk();
+    $response = $this->actingAs($user)->post('/app/tasks', ['name' => 'Test']);
+    $response->assertRedirect('app/tasks');
 
-        $response = $this->actingAs($user)->post('/app/tasks', ['name' => 'Test']);
-        $response->assertRedirect('app/tasks');
+    $task = Task::factory()->create();
+    $response = $this->actingAs($user)->put('/app/tasks/' . $task->id, ['name' => 'Test 2']);
+    $response->assertRedirect('app/tasks');
 
-        $task = Task::factory()->create();
-        $response = $this->actingAs($user)->put('/app/tasks/' . $task->id, ['name' => 'Test 2']);
-        $response->assertRedirect('app/tasks');
+    $this->assertDatabaseHas(Task::class, ['name' => 'Test 2']);
+    $response = $this->actingAs($user)->delete('/app/tasks/' . $task->id);
+    $response->assertRedirect('app/tasks');
+    $this->assertDatabaseMissing(Task::class, ['name' => 'Test 2']);
+});
 
-        $this->assertDatabaseHas(Task::class, ['name' => 'Test 2']);
-        $response = $this->actingAs($user)->delete('/app/tasks/' . $task->id);
-        $response->assertRedirect('app/tasks');
-        $this->assertDatabaseMissing(Task::class, ['name' => 'Test 2']);
-    }
+it('checks if tasks api crud is working', function () {
+    $user = User::factory()->create();
 
-    public function test_task_api_crud_is_working()
-    {
-        $user = User::factory()->create();
+    $response = $this->actingAs($user)->get('/api/v1/tasks');
+    $response->assertOk();
 
-        $response = $this->actingAs($user)->get('/api/v1/tasks');
-        $response->assertOk();
+    $response = $this->actingAs($user)->post('/api/v1/tasks', ['name' => 'Test']);
+    $response->assertCreated();
+    $this->assertDatabaseHas(Task::class, ['name' => 'Test']);
 
-        $response = $this->actingAs($user)->post('/api/v1/tasks', ['name' => 'Test']);
-        $response->assertCreated();
-        $this->assertDatabaseHas(Task::class, ['name' => 'Test']);
+    $task = Task::factory()->create();
+    $response = $this->actingAs($user)->put('/api/v1/tasks/' . $task->id, ['name' => 'Test 2']);
+    $response->assertOk();
+    $this->assertDatabaseHas(Task::class, ['name' => 'Test 2']);
 
-        $task = Task::factory()->create();
-        $response = $this->actingAs($user)->put('/api/v1/tasks/' . $task->id, ['name' => 'Test 2']);
-        $response->assertOk();
-        $this->assertDatabaseHas(Task::class, ['name' => 'Test 2']);
+    $response = $this->actingAs($user)->delete('/api/v1/tasks/' . $task->id);
+    $response->assertNoContent();
+    $this->assertDatabaseMissing(Task::class, ['name' => 'Test 2']);
+});
 
-        $response = $this->actingAs($user)->delete('/api/v1/tasks/' . $task->id);
-        $response->assertNoContent();
-        $this->assertDatabaseMissing(Task::class, ['name' => 'Test 2']);
-    }
+it('checks if admin middleware is working', function () {
+    $response = $this->get('/admin/dashboard');
+    $response->assertRedirect('login');
 
-    public function test_is_admin_middleware_is_working()
-    {
-        $response = $this->get('/admin/dashboard');
-        $response->assertRedirect('login');
+    $response = $this->get('/admin/stats');
+    $response->assertRedirect('login');
 
-        $response = $this->get('/admin/stats');
-        $response->assertRedirect('login');
+    $user = User::factory()->create();
 
-        $user = User::factory()->create();
+    $response = $this->actingAs($user)->get('/admin/dashboard');
+    $response->assertStatus(403);
 
-        $response = $this->actingAs($user)->get('/admin/dashboard');
-        $response->assertStatus(403);
+    $response = $this->actingAs($user)->get('/admin/stats');
+    $response->assertStatus(403);
 
-        $response = $this->actingAs($user)->get('/admin/stats');
-        $response->assertStatus(403);
+    $admin = User::factory()->create(['is_admin' => 1]);
 
-        $admin = User::factory()->create(['is_admin' => 1]);
+    $response = $this->actingAs($admin)->get('/admin/dashboard');
+    $response->assertViewIs('admin.dashboard');
 
-        $response = $this->actingAs($admin)->get('/admin/dashboard');
-        $response->assertViewIs('admin.dashboard');
-
-        $response = $this->actingAs($admin)->get('/admin/stats');
-        $response->assertViewIs('admin.stats');
-    }
-
-}
+    $response = $this->actingAs($admin)->get('/admin/stats');
+    $response->assertViewIs('admin.stats');
+});
