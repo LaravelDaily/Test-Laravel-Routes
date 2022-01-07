da89b9601e3f21ab586741f71e06cb72aeefb9b3
NaderElmor <47746617+NaderElmor@users.noreply.github.com>
2022-01-07 17:01:45 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
+use App\Http\Controllers\Admin\StatsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -72,7 +77,7 @@ Route::prefix('admin')->middleware(['is_admin'])->group(function () {
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::get('dashboard', DashboardController::class);
+        Route::get('dashboard', AdminDashboardController::class);
 
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
