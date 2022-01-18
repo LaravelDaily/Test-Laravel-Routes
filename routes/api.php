03beeb6231a794cc69c7b80c9a4815433723e9f3
Gleydson Silva <ghudson.ti@gmail.com>
2022-01-18 14:02:18 +0100

    Update api.php
    Using the correct TaskController

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\TaskController;
+use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,6 +24,5 @@ Route::group(['middleware' => 'auth:sanctum', 'prefix'=> 'v1'], function() {
     // Keep in mind that prefix should be /api/v1.
     // Add ONE line to assign 5 resource routes to TaskController
     // Put one code line here below
-    Route::resource('tasks', TaskController::class);
-
+    Route::apiResource('tasks', TaskController::class);
 });
