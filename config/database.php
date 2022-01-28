8dfc7b39d243dd8cbb59f3c0a896746e7634617e
Endrit Krasniqi <esl.endritkrasniqi@gmail.com>
2022-01-28 10:52:38 +0100

    Update database.php

--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'project2'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'spinp123@'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
