dfe45980d45069ef8a47aadc6762f53135ed5f9c
Ikram Khizer <ikramkhizar@gmail.com>
2022-01-31 16:30:22 +0500

    first attempt

--- routes/web.php
+++ routes/web.php
@@ -70,12 +70,12 @@ Route::group(['middleware'=>'auth'], function() {
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::get('/dashboard', DashboardController::class);
+        Route::get('/dashboard', AdminDashboardController::class);
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
         Route::get('/stats', StatsController::class);
-    });
+    };
     // End of the /admin Route Group
 });
 // End of the main Authenticated Route Group
