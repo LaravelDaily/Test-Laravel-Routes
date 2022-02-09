f656a1f16c6a29a6b3d95b6d24d856a1c007801f
Bruno Ferreira <brunohlferreira@gmail.com>
2022-02-09 04:54:53 +0000

    done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Illuminate\Database\Schema\Builder::defaultStringLength(240);
     }
 }
