730f63d7e7630f1912e2bbc49340d9216a2f1bc4
AnuragSingh299 <90097468+AnuragSingh299@users.noreply.github.com>
2022-02-15 12:35:05 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::get('/user/{name}', [UserController::class, 'show']);
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
-Route::redirect('/log-in', '/login');
+//Route::redirect('/log-in', '/login');
 
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
