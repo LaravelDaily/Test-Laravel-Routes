fb4e2c88622e376f432d9c6e60e3e443a88c6c97
Nwadialu Uchenna Gideon <62717218+Senseiuc@users.noreply.github.com>
2022-03-04 10:58:59 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
+
+fgagsjhs
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
