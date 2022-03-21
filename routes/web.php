5d34bbda661577793d13d1170f2d65b80ae0ccb4
Luca Di Fazio <highliuk@gmail.com>
2022-03-22 00:13:37 +0100

    Complete Task 4

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,7 @@ use App\Http\Controllers\UserController;
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/user/{name}', [UserController::class, 'show']);
 Route::view('/about', 'pages.about')->name('about');
-
-
-// Task 4: redirect the GET URL "log-in" to a URL "login"
-// Put one code line here below
+Route::redirect('/log-in', '/login');
 
 
 // Task 5: group the following route sentences below in Route::group()
