af07600482d493d9a37ada3093f1cee29c05063f
nikita <gorbacs.nikita@gmail.com>
2022-03-26 00:13:03 +0200

    Task controller API added

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
