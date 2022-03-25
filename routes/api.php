7b8f8c1ee673d8e60e33955952578985c6eafe2b
nikita <gorbacs.nikita@gmail.com>
2022-03-25 23:59:38 +0200

    4th testt

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,7 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     // Add ONE line to assign 5 resource routes to TaskController
     // Put one code line here below
 
-    Route::prefix('/v1', function () {
+    Route::prefix('/v1')->group(function () {
         Route::resource('/tasks', TaskController::class);
     });
 });
