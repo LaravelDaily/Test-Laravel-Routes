0518c8945fb73c71db1fe5b16d91d3d6c8a4e8b4
Admin <aziz89131983189@gmail.com>
2022-04-10 14:23:54 +0700

    fixed route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/app/dashboard';
-
+    //protected $namespace = 'App\Http\Controllers';
     /**
      * The controller namespace for the application.
      *
