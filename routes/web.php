27235d6cfe7ee6115ad3361711d216d9394dff49
Admin <aziz89131983189@gmail.com>
2022-04-10 15:46:46 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\StatsController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\DashboardController as DashBoard;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,21 +21,22 @@ use Illuminate\Support\Facades\Route;
 
 // Task 1: point the main "/" URL to the HomeController method "index"
 // Put one code line here below
-
+Route::get('/',[HomeController::class,'index']);
 
 // Task 2: point the GET URL "/user/[name]" to the UserController method "show"
 // It doesn't use Route Model Binding, it expects $name as a parameter
 // Put one code line here below
-
+Route::get('/user/{name}',[UserController::class,'show']);
 
 // Task 3: point the GET URL "/about" to the view
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-
+Route::view('/about','pages.about')->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
+Route::redirect('/log-in','/login');
 
 
 // Task 5: group the following route sentences below in Route::group()
@@ -56,6 +63,12 @@ use Illuminate\Support\Facades\Route;
 
     // End of the /app Route Group
 
+Route::middleware('auth')->group(function () {
+
+    Route::prefix('app')->group(function () {
+        Route::get('dashboard',DashBoard::class)->name('dashboard');
+        Route::get('tasks',TaskController::class);
+    });
 
     // Task 9: /admin group within a group
     // Add a group for routes with URL prefix "admin"
@@ -78,6 +91,10 @@ use Illuminate\Support\Facades\Route;
 
 // End of the main Authenticated Route Group
 
+    Route::group(['prefix' => 'admin', 'middleware' => "is_admin"], function() {
+        Route::get('dashboard',DashboardController::class);
+        Route::get('stats',StatsController::class);
+    });
 // One more task is in routes/api.php
-
+});
 require __DIR__.'/auth.php';
