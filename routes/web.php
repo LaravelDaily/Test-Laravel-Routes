771712e16fd9d94ce04d424c68b139fc7cfb0295
Khanh Chung <khanh.d.chung@gmail.com>
2022-06-06 01:31:44 -0700

    task 3

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/user/{name}', [UserController::class, 'show']);
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-
+Route::view('/about', 'pages.about')->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
