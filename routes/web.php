70ed8b91a68fa6bd08db27ba4ecd86c01d53cd3c
DatTran2000 <93904902+DatTran2000@users.noreply.github.com>
2022-06-06 15:41:07 +0700

    Update web.php
    testing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\StatsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,21 +20,26 @@ use Illuminate\Support\Facades\Route;
 
 // Task 1: point the main "/" URL to the HomeController method "index"
 // Put one code line here below
+Route::get('/', [HomeController::class,'index']);
 
 
 // Task 2: point the GET URL "/user/[name]" to the UserController method "show"
 // It doesn't use Route Model Binding, it expects $name as a parameter
 // Put one code line here below
+Route::get('/user/{name}', [UserController::class, 'show']);
 
 
 // Task 3: point the GET URL "/about" to the view
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-
+Route::get('/about', function(){
+    return view('pages.about');
+})->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
+Route::redirect('/log-in', '/login');
 
 
 // Task 5: group the following route sentences below in Route::group()
@@ -72,7 +82,17 @@ use Illuminate\Support\Facades\Route;
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
+    Route::middleware(['auth'])->group(function () {
+    Route::prefix('app')->group(function () {
+        Route::get('dashboard', DashboardController::class)->name('dashboard');
+        Route::resource('tasks', TaskController::class);
+    });
 
+    Route::middleware(['is_admin'])->prefix('admin')->group(function () {
+        Route::get('dashboard', DashboardController::class);
+        Route::get('stats' , StatsController::class);
+    });
+});
 
     // End of the /admin Route Group
 
