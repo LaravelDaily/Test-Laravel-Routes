0e767edad81f46d67441d91664ec55d987f435b8
DatTran2000 <dat.tran@gumiviet.com>
2022-06-06 14:53:42 +0700

    fix bug

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\Admin\StatsController;
 use Illuminate\Support\Facades\Route;
 
 /*
