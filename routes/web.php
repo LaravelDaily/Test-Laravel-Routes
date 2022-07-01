e2130057b407a79da80f0a7723bd8f3d8fda9f92
zuhriutama <zuhriutama@gmail.com>
2022-07-01 15:11:35 +0700

    bug fix case sensitive

--- routes/web.php
+++ routes/web.php
@@ -55,12 +55,12 @@ Route::middleware('auth')->group(function(){
         // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
         // Assign the route name "dashboard"
         // Put one Route Group code line here below
-        Route::get('dashboard', \App\Http\controllers\DashboardController::class)->name('dashboard');
+        Route::get('dashboard', \App\Http\Controllers\DashboardController::class)->name('dashboard');
 
         // Task 8: Manage tasks with URL /app/tasks/***.
         // Add ONE line to assign 7 resource routes to TaskController
         // Put one code line here below
-        Route::resource('tasks', \App\Http\controllers\TaskController::class);
+        Route::resource('tasks', \App\Http\Controllers\TaskController::class);
     });
     // End of the /app Route Group
 
@@ -76,12 +76,12 @@ Route::middleware('auth')->group(function(){
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::get('dashboard', \App\Http\controllers\Admin\DashboardController::class);
+        Route::get('dashboard', \App\Http\Controllers\Admin\DashboardController::class);
 
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
-        Route::get('stats', \App\Http\controllers\Admin\StatsController::class);
+        Route::get('stats', \App\Http\Controllers\Admin\StatsController::class);
 
     });
     // End of the /admin Route Group
