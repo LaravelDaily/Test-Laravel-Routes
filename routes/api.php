44e92e224af3c7389a1796558ba83521d249f166
SirMama-PHP <61635790+SirMama-PHP@users.noreply.github.com>
2022-07-09 17:06:54 +0600

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::group(['middleware' => 'auth:sanctum'], function() {
-    // Task 12: Manage tasks with endpoint /api/v1/tasks/*****.
-    // Keep in mind that prefix should be /api/v1.
-    // Add ONE line to assign 5 resource routes to TaskController
-    // Put one code line here below
-
+    Route::apiResource('v1/tasks', TaskController::class);
 });
+
