be9dea055a96611267e1119701bd9b774360a494
Robert Gonçalves Batista <robert.comunicar@gmail.com>
2022-07-10 10:59:29 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TaskController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
