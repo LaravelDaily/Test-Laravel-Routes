dad9f518d16c044d30349c421eee062de68c2e9a
alpernuage <alperakbulut@gmail.com>
2022-07-19 00:39:17 +0200

    Check Test Laravel Routes, update route files

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,9 @@ Route::get('/user/{name}', [UserController::class, 'show']);
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-Route::view('/pages/about', 'about')->name('about');
+Route::get('/about', function () {
+    return view('pages.about');
+})->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
