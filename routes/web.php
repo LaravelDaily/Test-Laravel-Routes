1a0ddd31876001759d653de5d0ce80fff1113545
alpernuage <alperakbulut@gmail.com>
2022-07-19 00:36:20 +0200

    Check Test Laravel Routes, remove .env file

--- routes/web.php
+++ routes/web.php
@@ -56,12 +56,12 @@ Route::middleware(['auth'])->group(function () {
 // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
 // Assign the route name "dashboard"
 // Put one Route Group code line here below
-        Route::get('/app/dashboard', [DashboardController::class])->name('dashboard');
+        Route::get('/dashboard', [DashboardController::class])->name('dashboard');
 
 // Task 8: Manage tasks with URL /app/tasks/***.
 // Add ONE line to assign 7 resource routes to TaskController
 // Put one code line here below
-        Route::resource('/app/tasks/***', TaskController::class);
+        Route::resource('/tasks', TaskController::class);
     });
 // End of the /app Route Group
 
@@ -77,11 +77,11 @@ Route::middleware(['auth'])->group(function () {
 
 // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
 // Put one code line here below
-        Route::get('/admin/dashboard', [AdminDashboardController::class]);
+        Route::get('/dashboard', [AdminDashboardController::class]);
 
 // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
 // Put one code line here below
-        Route::get('/admin/stats', [StatsController::class]);
+        Route::get('/stats', [StatsController::class]);
 
     });
 // End of the /admin Route Group
