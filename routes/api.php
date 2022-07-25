8025547e882a6d7c8c1e7e9b7a1b95c061e6755c
Saad El Gabouri <elgabouri.saad@gmail.com>
2022-07-25 20:33:36 +0100

    fix the api routing

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,6 @@ Route::group(['middleware' => 'auth:sanctum'], function() {
     // Keep in mind that prefix should be /api/v1.
     // Add ONE line to assign 5 resource routes to TaskController
     // Put one code line here below
-    Route::apiResource('/api/v1/tasks', TaskController::class);
+    Route::apiResource('v1/tasks', TaskController::class);
 
 });
