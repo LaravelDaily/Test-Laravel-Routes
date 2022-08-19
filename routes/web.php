1938e11b902b9e6298640bfd686ea625d7b55910
MuhammedMukdad <muhammednmukdad1999m@gmail.com>
2022-08-19 15:54:28 -0700

    solve tasks111

--- routes/web.php
+++ routes/web.php
@@ -76,11 +76,11 @@ Route::group(['middleware'=>'auth'],function(){
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
 
-        Route::get('dashboard', 'DashboardController');
+        Route::get('/dashboard', DashboardController::class);
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
 
-        Route::get('stats', 'StatsController');
+        Route::get('/stats', StatsController::class);
     // End of the /admin Route Group
     });
 // End of the main Authenticated Route Group
