ca8b186fcab120626ed302b6a6a262a265082578
sohail Ahmad <sohailahmad0757@gmail.com>
2022-09-07 01:37:04 +0500

    All Test Passed

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,90 +4,91 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class TaskController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
         $tasks = Task::all();
 
-        return view('tasks.index', compact('tasks'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('tasks.create');
+        return view("tasks.index", compact("tasks"));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
-        Task::create($request->validate(['name' => 'required']));
+        Task::create($request->validate(["name" => "required"]));
 
-        return redirect()->route('tasks.index');
+        return redirect()->route("tasks.index");
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        return view("tasks.create");
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
+     * @param Task $task
+     * @return Response
      */
     public function show(Task $task)
     {
-        return view('tasks.show', compact('task'));
+        return view("tasks.show", compact("task"));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
+     * @param Task $task
+     * @return Response
      */
     public function edit(Task $task)
     {
-        return view('tasks.edit', compact('task'));
+        return view("tasks.edit", compact("task"));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Task $task
+     * @return Response
      */
     public function update(Request $request, Task $task)
     {
-        $task->update($request->validate(['name' => 'required']));
+        $task->update($request->validate(["name" => "required"]));
 
-        return redirect()->route('tasks.index');
+        return redirect()->route("tasks.index");
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Task  $task
-     * @return \Illuminate\Http\Response
+     * @param Task $task
+     * @return Response
      */
     public function destroy(Task $task)
     {
         $task->delete();
 
-        return redirect()->route('tasks.index');
+        return redirect()->route("tasks.index");
     }
 }
