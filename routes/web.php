f6d91fb63fd1cf608d6a224e3bc5e68413885170
ErikasAbukauskas <85123397+ErikasAbukauskas@users.noreply.github.com>
2022-09-08 16:26:58 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
