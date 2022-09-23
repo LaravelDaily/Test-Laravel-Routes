afe0fa5ab7245123c9e986cc5074e1b92ddcd9e8
Priceless-P <prisca.priceless11@gmail.com>
2022-09-23 18:49:49 +0100

    correction

--- routes/web.php
+++ routes/web.php
@@ -45,12 +45,12 @@ Route::prefix('/app')->group(function() {
         // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
         // Assign the route name "dashboard"
         // Put one Route Group code line here below
-Route::get('dashboard', [DashboardController::class])->name('dashboard')
+Route::get('dashboard', [DashboardController::class])->name('dashboard');
 
         // Task 8: Manage tasks with URL /app/tasks/***.
         // Add ONE line to assign 7 resource routes to TaskController
         // Put one code line here below
-Route::resource('tasks', TaskController);
+Route::resource('tasks', TaskController::class);
     // End of the /app Route Group
                          });
     // Task 9: /admin group within a group
@@ -64,14 +64,14 @@ Route::group(['middleware'=>'is_admin', 'prefix'=> 'admin'])->group(function() {
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-Route::get('/dashboard', DashboardController::class)
+Route::get('/dashboard', DashboardController::class);
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
 Route::get('/stats', StatsController::class)
 
     // End of the /admin Route Group
-    });
+});
 // End of the main Authenticated Route Group
  });
 // One more task is in routes/api.php
