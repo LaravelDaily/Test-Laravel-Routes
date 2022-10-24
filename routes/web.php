128c071b0ef4dedfc95dd0d4c8503dc2816674dc
mithuncdas.cse <mithuncdas.cse@gmail.com>
2022-10-25 00:39:47 +0600

    speeling mistake change

--- routes/web.php
+++ routes/web.php
@@ -61,7 +61,7 @@ Route::group(['middleware' => 'auth'],function(){
         // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
         // Assign the route name "dashboard"
         // Put one Route Group code line here below
-        Route::get('/dashbaord',DashboardController::class)->name('dashboard');
+        Route::get('/dashboard',DashboardController::class)->name('dashboard');
 
         // Task 8: Manage tasks with URL /app/tasks/***.
         // Add ONE line to assign 7 resource routes to TaskController
