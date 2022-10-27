e39dad9e9d21044166a05513a89d6fd8b97d436f
= <anton.graham.2011@gmail.com>
2022-10-27 02:39:52 -0400

    update

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,9 @@ Route::get('user/{user}',[UserController::class,'show']);
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-Route::get('/about','pages.about')->name('about');
+Route::get('/about',function(){
+    return view('pages.about');
+})->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
