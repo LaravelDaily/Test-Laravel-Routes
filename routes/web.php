f528bf4b6d3a427d1bafe94a06ab64f791565f1d
Salauat <salauat.kabir02@gmail.com>
2022-10-28 17:52:01 +0600

    all

--- routes/web.php
+++ routes/web.php
@@ -76,7 +76,7 @@ Route::prefix('admin')->middleware('is_admin')->group(function (){
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-Route::get('/dashboard',DashboardController::class);
+Route::get('/dashboard',\App\Http\Controllers\Admin\DashboardController::class);
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
