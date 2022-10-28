a8efb5978c05b8b73897d0ca9ecf9922ccd5044b
Salauat <salauat.kabir02@gmail.com>
2022-10-28 15:41:41 +0600

    task 4

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::view('/about','about')->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
-
+Route::redirect('/log-in','/login');
 
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
