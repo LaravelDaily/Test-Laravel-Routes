4dca4529c414dceba11853ca2840ead3529f87c2
nkwuda theophilus <theophilusonyebuchinkwuda@gmail.com>
2022-11-07 12:35:48 +0100

    quick fix

--- routes/web.php
+++ routes/web.php
@@ -82,11 +82,11 @@ Route::group(['middleware' => 'auth'],function(){
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-            Route::get('/dashboard',[DashboardController::class]);
+            Route::get('/dashboard',DashboardController::class);
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
-            Route::get('/stats',[DashboardController::class]);
+            Route::get('/stats',DashboardController::class);
     });
     // End of the /admin Route Group
 });
