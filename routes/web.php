334bdbc4bea959438795573573c9c9f5dc2abaa3
Nkwuda Theophilus <theophilusonyebuchinkwuda@gmail.com>
2022-11-07 10:38:49 +0100

    quick fix

--- routes/web.php
+++ routes/web.php
@@ -60,7 +60,7 @@ Route::middleware(['auth'])->group(function () {
         // Task 8: Manage tasks with URL /app/tasks/***.
         // Add ONE line to assign 7 resource routes to TaskController
         // Put one code line here below
-        Route::resource('/tasks', TaskController::class);
+        Route::resource('/tasks', TaskContoller::class);
     });
     // End of the /app Route Group
 
