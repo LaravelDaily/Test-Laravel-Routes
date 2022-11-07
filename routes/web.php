43755552542ea1d33c1b97b4bba4fad902cf5998
nkwuda theophilus <theophilusonyebuchinkwuda@gmail.com>
2022-11-07 12:29:26 +0100

    quick fix

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\DashboardController as ControllersDashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -55,7 +56,7 @@ Route::group(['middleware' => 'auth'],function(){
         // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
         // Assign the route name "dashboard"
         // Put one Route Group code line here below
-        Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class])->name('dashboard');
+        Route::get('/dashboard', ControllersDashboardController::class)->name('dashboard');
 
         // Task 8: Manage tasks with URL /app/tasks/***.
         // Add ONE line to assign 7 resource routes to TaskController
