65843afcae188f3b223e3fa612ed93efa30f6422
Khanh Chung <khanh.d.chung@gmail.com>
2022-11-12 21:11:41 -0800

    Task 9, 10, 11 completed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use \App\Http\Controllers\Admin;
+
 use \App\Http\Controllers\TaskController;
 use \App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
@@ -70,22 +72,21 @@ Route::middleware(['auth'])->group(function() {
     // Add a group for routes with URL prefix "admin"
     // Assign middleware called "is_admin" to them
     // Put one Route Group code line here below
+Route::prefix('admin')->middleware(['is_admin'])->group(function() {
+    // Tasks inside that /admin group:
 
+    // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
+    // Put one code line here below
+    Route::get('/dashboard', Admin\DashboardController::class);
 
-        // Tasks inside that /admin group:
-
-
-        // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
-        // Put one code line here below
-
-
-        // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
-        // Put one code line here below
-
+    // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
+    // Put one code line here below
+    Route::get('/stats', Admin\StatsController::class);
 
     // End of the /admin Route Group
 
 // End of the main Authenticated Route Group
+});
 
 // One more task is in routes/api.php
 
