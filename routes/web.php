ce2a05ec17191c9c1820dc22279edb00b1ff6419
Anwar Aji <96333744+anwarajijuloh@users.noreply.github.com>
2022-11-15 00:57:24 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,47 +15,47 @@ use Illuminate\Support\Facades\Route;
 
 // Task 1: point the main "/" URL to the HomeController method "index"
 // Put one code line here below
-
+Route::get('/', [HomeController::class, 'index']);
 
 // Task 2: point the GET URL "/user/[name]" to the UserController method "show"
 // It doesn't use Route Model Binding, it expects $name as a parameter
 // Put one code line here below
-
+Route::get('/user/{name}', [UserController::class, 'show']);
 
 // Task 3: point the GET URL "/about" to the view
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-
+Route::get('/about', fn() => view('pages.about'); )->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
-
+Route::redirect('/log-in', '/login');
 
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
 // Put one Route Group code line here below
-
+Route::group(['middleware' => 'auth'], function() {
     // Tasks inside that Authenticated group:
 
     // Task 6: /app group within a group
     // Add another group for routes with prefix "app"
     // Put one Route Group code line here below
-
+       Route::prefix('app')->group(function() {
         // Tasks inside that /app group:
-
+           Route::get('/app', function() {});
 
         // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
         // Assign the route name "dashboard"
         // Put one Route Group code line here below
-
+           Route::get('/app/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
         // Task 8: Manage tasks with URL /app/tasks/***.
         // Add ONE line to assign 7 resource routes to TaskController
         // Put one code line here below
-
+           Route::resource('tasks', 'TaskController');
     // End of the /app Route Group
-
+       });
 
     // Task 9: /admin group within a group
     // Add a group for routes with URL prefix "admin"
