c396043206e4dda8740611c6136960eb7fbd9c1a
slawek-maslowski <60491600+slawek-maslowski@users.noreply.github.com>
2022-11-17 13:04:20 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,25 @@ use Illuminate\Support\Facades\Route;
 
 // Task 1: point the main "/" URL to the HomeController method "index"
 // Put one code line here below
+Route::get('/', [HomeController::class, 'index']);
 
 
 // Task 2: point the GET URL "/user/[name]" to the UserController method "show"
 // It doesn't use Route Model Binding, it expects $name as a parameter
 // Put one code line here below
+Route::get('user/{name}', [UserController::class, 'show']);
 
 
 // Task 3: point the GET URL "/about" to the view
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
+Route::view('/about', 'pages.about')->name('about');
 
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
+Route::redirect('log-in', 'login');
 
 
 // Task 5: group the following route sentences below in Route::group()
