0f31b0d8f9ae1b1980e0f876778c92d862985ba7
sandeepstha46 <sandeepstha64@gmail.com>
2022-12-10 20:15:54 +0545

    [Fix] invoke method missing

--- routes/web.php
+++ routes/web.php
@@ -62,7 +62,7 @@ Route::group(['prefix' => 'app'], function (){
 // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
 // Assign the route name "dashboard"
 // Put one Route Group code line here below
-Route::get('/dashboard', DashboardController::class)->name('dashboard');
+Route::get('/dashboard', DashboardController::class, '__invoke')->name('dashboard');
 
 
 // Task 8: Manage tasks with URL /app/tasks/***.
