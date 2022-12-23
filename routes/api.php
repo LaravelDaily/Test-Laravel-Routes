20b49db9dcadf15660640865a1c20973d5d22e62
Jihad92 <41027863+Jihad92@users.noreply.github.com>
2022-12-23 21:02:47 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-@@ -23,5 +24,7 @@
     // Keep in mind that prefix should be /api/v1.
     // Add ONE line to assign 5 resource routes to TaskController
     // Put one code line here below
