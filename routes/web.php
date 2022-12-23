6d7deb93468e8e2de99ee3f1c14a7f75d9e247cc
Jihad92 <41027863+Jihad92@users.noreply.github.com>
2022-12-23 20:47:37 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -59,13 +59,13 @@ Route::middleware('auth')->group(function () {
         // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
         // Assign the route name "dashboard"
         // Put one Route Group code line here below
-        Route::get('/dashboard', DashboardController::class)->name('dashboard');
+        Route::get('/dashboard', [DashboardController::class])->name('dashboard');
 
 
         // Task 8: Manage tasks with URL /app/tasks/***.
         // Add ONE line to assign 7 resource routes to TaskController
         // Put one code line here below
-        Route::resource('/tasks', TaskController::class);
+        Route::resource('/tasks', [TaskController::class]);
 
     });
     // End of the /app Route Group
@@ -83,12 +83,12 @@ Route::middleware('auth')->group(function () {
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::get('/dashbourd', DashboardController::class);
+        Route::get('/dashbourd', [DashboardController::class]);
 
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
-        Route::get('/stats', StatsController::class);
+        Route::get('/stats', [StatsController::class]);
 
 
     });
