2838b02ac53a3010d35587a23cf15de53a234016
Yeraldi Mujica <94128962+Yeraldi29@users.noreply.github.com>
2023-01-25 19:05:53 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use App\Http\Controllers\Admin\StatsController;
-use App\Http\Controllers\Api\V1\TaskController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -45,7 +45,7 @@ Route::redirect('/log-in', 'login');
 // Assign middleware "auth"
 // Put one Route Group code line here below
 // Tasks inside that Authenticated group:
-Route::middleware(['auth'])->group(function () {
+Route::middleware('auth')->group(function () {
 
     // Task 6: /app group within a group
     // Add another group for routes with prefix "app"
@@ -71,7 +71,7 @@ Route::middleware(['auth'])->group(function () {
     // Add a group for routes with URL prefix "admin"
     // Assign middleware called "is_admin" to them
     // Put one Route Group code line here below
-    Route::group(['prefix' => 'admin', 'middleware' => 'is_admin'], function () {
+    Route::prefix('admin')->middleware('is_admin')->group(function () {
         // Tasks inside that /admin group:
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
@@ -80,12 +80,12 @@ Route::middleware(['auth'])->group(function () {
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
-        Route::get('stats',[StatsController::class]);
+        Route::get('stats', [StatsController::class]);
 
         // End of the /admin Route Group
+        
     });
-
-    // End of the main Authenticated Route Group
+        // End of the main Authenticated Route Group
 });
 // One more task is in routes/api.php
 
