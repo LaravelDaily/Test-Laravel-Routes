9f7e6a8ec960327e98b22d80bb2626c90dc43ccd
Dereck Vinicius Silva <70153036+DereckSilva@users.noreply.github.com>
2023-01-29 14:04:08 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 
 /*
