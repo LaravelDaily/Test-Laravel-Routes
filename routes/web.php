8fe581c5dc00b66e43403f8232ff4d9b155263a9
Abdelrhman Abouelez <Abouelez10@gmail.com>
2023-02-06 17:00:20 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,92 +1,28 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController as AdminDashboard;
-use App\Http\Controllers\Admin\StatsController;
-use App\http\Controllers\DashboardController as DashboardCtr;
-use App\Http\Controllers\Api\V1\TaskController;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\Api\V1\TaskController as V1TaskController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
+| Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-// Task 1: point the main "/" URL to the HomeController method "index"
-// Put one code line here below
-Route::get('/', [HomeController::class, 'index']);
-
-// Task 2: point the GET URL "/user/[name]" to the UserController method "show"
-// It doesn't use Route Model Binding, it expects $name as a parameter
-// Put one code line here below
-Route::get('/users/{name}', [UserController::class, 'show']);
-
-// Task 3: point the GET URL "/about" to the view
-// resources/views/pages/about.blade.php - without any controller
-// Also, assign the route name "about"
-// Put one code line here below
-Route::view('/about', 'pages.about')->name('about');
-
-// Task 4: redirect the GET URL "log-in" to a URL "login"
-// Put one code line here below
-Route::redirect('log-in', 'login');
-// Task 5: group the following route sentences below in Route::group()
-// Assign middleware "auth"
-// Put one Route Group code line here below
-Route::middleware(['auth'])->prefix('app')->group(function () {
-
-    // Tasks inside that Authenticated group:
-
-    // Task 6: /app group within a group
-    // Add another group for routes with prefix "app"
-    // Put one Route Group code line here below
-
-    // Tasks inside that /app group:
-
-
-    // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
-    // Assign the route name "dashboard"
-    // Put one Route Group code line here below
-
-    Route::get('/dashboard', 'DashboardCtr@invoke')->name('dashboard');
-
-    // Task 8: Manage tasks with URL /app/tasks/***.
-    // Add ONE line to assign 7 resource routes to TaskController
-    // Put one code line here below
-    Route::resource('/tasks', 'TaskController');
-    // End of the /app Route Group
-
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
 });
 
-// Task 9: /admin group within a group
-// Add a group for routes with URL prefix "admin"
-// Assign middleware called "is_admin" to them
-// Put one Route Group code line here below
-Route::prefix('admin')->middleware(['is_admin'])->group(function () {
-
-
-    // Tasks inside that /admin group:
-
-
-    // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
+Route::group(['middleware' => 'auth:sanctum', 'prefix' => 'v1'], function () {
+    // Task 12: Manage tasks with endpoint /api/v1/tasks/*****.
+    // Keep in mind that prefix should be /api/v1.
+    // Add ONE line to assign 5 resource routes to TaskController
     // Put one code line here below
-    Route::get('dashboard', AdminDashboard::class);
-
-    // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
-    // Put one code line here below
-    Route::get('stats', StatsController::class);
-
-    // End of the /admin Route Group
+    Route::apiResource('tasks', V1TaskController::class);
 });
-// End of the main Authenticated Route Group
-
-// One more task is in routes/api.php
-
-require __DIR__ . '/auth.php';
