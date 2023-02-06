7e16cbea556f33281d0e58dcd4651561eb110014
Abdelrhman Abouelez <Abouelez10@gmail.com>
2023-02-06 16:59:40 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController as AdminDashboard;
+use App\Http\Controllers\Admin\StatsController;
+use App\http\Controllers\DashboardController as DashboardCtr;
+use App\Http\Controllers\Api\V1\TaskController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,26 +21,26 @@ use Illuminate\Support\Facades\Route;
 
 // Task 1: point the main "/" URL to the HomeController method "index"
 // Put one code line here below
-
+Route::get('/', [HomeController::class, 'index']);
 
 // Task 2: point the GET URL "/user/[name]" to the UserController method "show"
 // It doesn't use Route Model Binding, it expects $name as a parameter
 // Put one code line here below
-
+Route::get('/users/{name}', [UserController::class, 'show']);
 
 // Task 3: point the GET URL "/about" to the view
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-
+Route::view('/about', 'pages.about')->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
-
-
+Route::redirect('log-in', 'login');
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
 // Put one Route Group code line here below
+Route::middleware(['auth'])->prefix('app')->group(function () {
 
     // Tasks inside that Authenticated group:
 
@@ -42,42 +48,45 @@ use Illuminate\Support\Facades\Route;
     // Add another group for routes with prefix "app"
     // Put one Route Group code line here below
 
-        // Tasks inside that /app group:
+    // Tasks inside that /app group:
 
 
-        // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
-        // Assign the route name "dashboard"
-        // Put one Route Group code line here below
-
-
-        // Task 8: Manage tasks with URL /app/tasks/***.
-        // Add ONE line to assign 7 resource routes to TaskController
-        // Put one code line here below
-
-    // End of the /app Route Group
-
-
-    // Task 9: /admin group within a group
-    // Add a group for routes with URL prefix "admin"
-    // Assign middleware called "is_admin" to them
+    // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
+    // Assign the route name "dashboard"
     // Put one Route Group code line here below
 
+    Route::get('/dashboard', 'DashboardCtr@invoke')->name('dashboard');
 
-        // Tasks inside that /admin group:
+    // Task 8: Manage tasks with URL /app/tasks/***.
+    // Add ONE line to assign 7 resource routes to TaskController
+    // Put one code line here below
+    Route::resource('/tasks', 'TaskController');
+    // End of the /app Route Group
+
+});
+
+// Task 9: /admin group within a group
+// Add a group for routes with URL prefix "admin"
+// Assign middleware called "is_admin" to them
+// Put one Route Group code line here below
+Route::prefix('admin')->middleware(['is_admin'])->group(function () {
 
 
-        // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
-        // Put one code line here below
+    // Tasks inside that /admin group:
 
 
-        // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
-        // Put one code line here below
+    // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
+    // Put one code line here below
+    Route::get('dashboard', AdminDashboard::class);
 
+    // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
+    // Put one code line here below
+    Route::get('stats', StatsController::class);
 
     // End of the /admin Route Group
-
+});
 // End of the main Authenticated Route Group
 
 // One more task is in routes/api.php
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
