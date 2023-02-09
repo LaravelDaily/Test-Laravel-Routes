be12e53c977a74b745636a83f0966488e41a2d4e
nnivxix <adhe.hanasa@gmail.com>
2023-02-09 08:45:23 +0700

    add route API

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ Route::redirect("/log-in", "/login");
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
 // Put one Route Group code line here below
-Route::group(['middleware' => ['auth']], function () {
+Route::group(['middleware' => 'auth'], function () {
 
     // Tasks inside that Authenticated group:
 
@@ -58,7 +58,7 @@ Route::group(['middleware' => ['auth']], function () {
       // Task 8: Manage tasks with URL /app/tasks/***.
       // Add ONE line to assign 7 resource routes to TaskController
       // Put one code line here below
-      Route::resource("/tasks", TaskController::class);
+      Route::resource("tasks", TaskController::class);
 
       // End of the /app Route Group
     });
@@ -75,11 +75,11 @@ Route::group(['middleware' => ['auth']], function () {
 
       // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
       // Put one code line here below
-      Route::get("/dashboard", AdminDashboardController::class);
+      Route::get("dashboard", AdminDashboardController::class);
 
       // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
       // Put one code line here below
-      Route::get("/stats", AdminStatsController::class);
+      Route::get("stats", AdminStatsController::class);
 
 
       // End of the /admin Route Group
