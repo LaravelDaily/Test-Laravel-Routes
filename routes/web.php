253baa892521c94a5f6ba3e583803bf9a250f8fb
Shudhamsu <shresthashudhamsu@gmail.com>
2023-02-12 11:43:54 +1100

    completed routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\Admin\DashboardController as DashAdminController;
+use App\Http\Controllers\Admin\StatsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,27 +21,41 @@ use Illuminate\Support\Facades\Route;
 
 // Task 1: point the main "/" URL to the HomeController method "index"
 // Put one code line here below
+Route::get('/',[HomeController::class, 'index']);
 
 
 // Task 2: point the GET URL "/user/[name]" to the UserController method "show"
 // It doesn't use Route Model Binding, it expects $name as a parameter
 // Put one code line here below
+Route::get('/user/{name}', [UserController::class, 'show']);
 
 
 // Task 3: point the GET URL "/about" to the view
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
+Route::get('/about', function(){
+    return view('pages.about');
+});
 
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
-
+Route::redirect('log-in','login');
 
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
 // Put one Route Group code line here below
+Route::middleware('auth')->group(function(){
+    Route::prefix('app')->group(function(){
+        Route::get('/dashboard',[DashboardController::class]);
+//        Route::prefix('tasks')->group(function(){
+//            Route::resource()
+//        });
+        Route::resource('tasks',TaskController::class );
+    });
 
+});
     // Tasks inside that Authenticated group:
 
     // Task 6: /app group within a group
@@ -72,6 +92,10 @@ use Illuminate\Support\Facades\Route;
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
+    Route::middleware('is_admin')->prefix('admin')->group(function(){
+        Route::get('Admin.dashboard',[DashAdminController::class]);
+        Route::get('Admin.stats',[StatsController::class]);
+    });
 
 
     // End of the /admin Route Group
