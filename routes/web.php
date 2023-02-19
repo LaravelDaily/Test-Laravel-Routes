c4fac3384222ae3fabea942548380511966b3812
Andrey Meshkov <ameshkov@onevision.kz>
2023-02-19 15:08:33 +0600

    Completed all web tasks

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
+use App\Http\Controllers\Admin\StatsController as AdminStatsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -66,20 +68,19 @@ Route::middleware(['auth'])->group(function () {
     // Add a group for routes with URL prefix "admin"
     // Assign middleware called "is_admin" to them
     // Put one Route Group code line here below
-
-
+    Route::middleware('is_admin')->prefix('admin')->group(function () {
         // Tasks inside that /admin group:
-
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-
+        Route::get('/dashboard', AdminDashboardController::class);
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
-
+        Route::get('/stats', AdminStatsController::class);
 
     // End of the /admin Route Group
+    });
 // End of the main Authenticated Route Group
 });
 
