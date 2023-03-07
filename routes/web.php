214f4eb67608b9d8b52a34040a726b1bc74b5a2c
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2023-03-07 17:44:14 -0300

    Fix Task 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,7 +19,7 @@ Route::get('/', [HomeController::class, 'index']);
 // Task 1: point the main "/" URL to the HomeController method "index"
 // Put one code line here below
 
-
+Route::get('/user/{name}', [UserController::class, 'show']);
 // Task 2: point the GET URL "/user/[name]" to the UserController method "show"
 // It doesn't use Route Model Binding, it expects $name as a parameter
 // Put one code line here below
