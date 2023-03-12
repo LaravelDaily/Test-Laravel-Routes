5b9c70f18efebdfcd9e16b055632b7859bf87643
Xafy <57871519+Xafy@users.noreply.github.com>
2023-03-12 22:13:57 +0200

    ff

--- routes/web.php
+++ routes/web.php
@@ -78,21 +78,8 @@ Route::group(['middleware' => 'is_admin', 'prefix' => 'admin'], function (){
     // End of the /admin Route Group
 });
 
-    // Task 9: /admin group within a group
-    // Add a group for routes with URL prefix "admin"
-    // Assign middleware called "is_admin" to them
-    // Put one Route Group code line here below
-Route::group(['middleware' => 'is_admin', 'prefix' => 'admin'], function (){
-// Tasks inside that /admin group:
-
-// Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
-// Put one code line here below
-    Route::get('/dashboard', AdminDashboardController::class);
-// Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
-// Put one code line here below
-    Route::get('stats', StatsController::class);
-// End of the /admin Route Group
 });
+
 
 
 // End of the main Authenticated Route Group
