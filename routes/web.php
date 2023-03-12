2524dbff1aa0ff1776561c1e9b9ab664df21dcc9
Eburu Dearie <eburujacintha@gmail.com>
2023-03-13 00:26:32 +0200

    Fixed bug

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
-use App\http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Admin\StatsController;
+use App\Http\Controllers\Api\V1\TaskController;
 use App\Http\Controllers\Admin\DashboardController as AdminDashboard;
 
 /*
