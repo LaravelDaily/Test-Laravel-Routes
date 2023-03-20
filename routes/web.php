24552d253abf68443602e9bcd0b286711a8e9ece
Andranik Balasanyan <44621471+androneg@users.noreply.github.com>
2023-03-20 19:41:27 +0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
