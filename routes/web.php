52a744e79f8d5febb80fe0ed4dda6f1330f46052
si-eng <brownkida79@gmail.com>
2023-03-23 13:59:10 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,15 @@ Route::get('/', [HomeController::class, 'index']);
 // Task 2: point the GET URL "/user/[name]" to the UserController method "show"
 // It doesn't use Route Model Binding, it expects $name as a parameter
 // Put one code line here below
-
+Route::get('/user/{name}', [HomeController::class, 'show']);
 
 // Task 3: point the GET URL "/about" to the view
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-
+Route::get('/about', function(){
+    return view('pages.about')->name('about');
+});
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
