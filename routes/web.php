ac7f7d6601ff1b8da438b350a4d3ab34dd0285cf
Andranik Balasanyan <44621471+androneg@users.noreply.github.com>
2023-03-23 10:50:29 +0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::redirect('log-in', 'login');
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
 // Put one Route Group code line here below
-Route::middleware(['auth'])->group(function () {
+Route::group(['middleware' => 'auth'], function() {    
     // Tasks inside that Authenticated group:
 
     // Task 6: /app group within a group
