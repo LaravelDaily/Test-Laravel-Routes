d4edaf3f6619379459e78c599e959e45e6c84c18
si-eng <brownkida79@gmail.com>
2023-03-23 14:31:19 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TaskController;
-use App\Http\Controllers\StatsController;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\StatsController;
 
 /*
 |--------------------------------------------------------------------------
