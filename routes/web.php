6d32429a0409b05b1179a5e7fc63a817b84f9520
Ivan Golubev <golubevivan0500@gmail.com>
2023-04-09 11:39:29 +0300

    Task 11 complete

--- routes/web.php
+++ routes/web.php
@@ -82,6 +82,7 @@ Route::middleware('auth')->group(function() {
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
+        Route::get('/stats', StatsController::class);
 
 
     // End of the /admin Route Group
