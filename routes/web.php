cb555f4df26fc8790f76286bf47ec5d8ea1a715c
Ivan Golubev <golubevivan0500@gmail.com>
2023-04-09 11:38:44 +0300

    Task 9 complete

--- routes/web.php
+++ routes/web.php
@@ -70,7 +70,7 @@ Route::middleware('auth')->group(function() {
     // Add a group for routes with URL prefix "admin"
     // Assign middleware called "is_admin" to them
     // Put one Route Group code line here below
-
+    Route::group(['prefix' => 'admin', 'middleware' => 'is_admin'], function() {
 
         // Tasks inside that /admin group:
 
@@ -84,6 +84,7 @@ Route::middleware('auth')->group(function() {
 
 
     // End of the /admin Route Group
+    });
 
 // End of the main Authenticated Route Group
 });
