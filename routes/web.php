18ba6f253a768f63dda7ee5f03d05faa8f9e2c11
Ivan Golubev <golubevivan0500@gmail.com>
2023-04-09 11:32:31 +0300

    Task 2 complete

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', [HomeController::class, 'index']);
 // Task 2: point the GET URL "/user/[name]" to the UserController method "show"
 // It doesn't use Route Model Binding, it expects $name as a parameter
 // Put one code line here below
+Route::get('/user/{name}', [UserController::class, 'show']);
 
 
 // Task 3: point the GET URL "/about" to the view
