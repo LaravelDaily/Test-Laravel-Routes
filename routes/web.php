669b267dd472bc3c325853b3b88684b53968dbce
alloyking1 <alloyking1@gmail.com>
2023-04-10 23:04:19 +0100

    all test cases passed

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-// use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Admin\StatsController;
 use App\Http\Controllers\HomeController;
@@ -79,7 +79,7 @@ Route::middleware('auth')->group(function () {
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
 
-        Route::get('/dashboard', DashboardController::class);
+        Route::get('/dashboard', AdminDashboardController::class);
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
