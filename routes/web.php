912291f92e23ccbcaa21d475b094271207649436
aonsi55 <aalifahmy@alexu.edu.eg>
2023-04-23 23:47:44 +0200

    Fix for failed test run

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,19 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
-
-// 
-// |--------------------------------------------------------------------------
-// | Web Routes
-// |--------------------------------------------------------------------------
-// |
-// | Here is where you can register web routes for your application. These
-// | routes are loaded by the RouteServiceProvider within a group which
-// | contains the "web" middleware group. Now create something great!
-// |
-
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 // Task 1: point the main "/" URL to the HomeController method "index"
 // Put one code line here below
