8c165d64e9b549191d3961d8d8018a38c95ac39d
Spawn2k <121321391+Spawn2k@users.noreply.github.com>
2023-04-29 17:33:01 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
