2fac76e28de94595e2700be44d432e3bb2a42d62
Abdullah Wasif <aef1803@gmail.com>
2023-05-20 15:11:17 +0600

    userController path added

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Admin\DashboardController as adminDashboard;
 use App\Http\Controllers\Admin\StatsController;
