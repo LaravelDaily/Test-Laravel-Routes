8899db14c1179627482c4f9fdb0155169b991ee5
Rubel-CSTE <rubel.nstu27@gmail.com>
2023-05-31 03:15:27 +0600

    Laravel Routing Skills

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::middleware(['auth'])->group(function(){
         // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
         // Assign the route name "dashboard"
         // Put one Route Group code line here below
-        Route::post('/dashboard', \App\Http\Controllers\DashboardController::class)->name('dashboard');
+        Route::get('/dashboard', \App\Http\Controllers\DashboardController::class)->name('dashboard');
 
         // Task 8: Manage tasks with URL /app/tasks/***.
         // Add ONE line to assign 7 resource routes to TaskController
@@ -68,11 +68,11 @@ Route::middleware(['auth'])->group(function(){
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::post('/dashboard', \App\Http\Controllers\Admin\DashboardController::class);
+        Route::get('/dashboard', \App\Http\Controllers\Admin\DashboardController::class);
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
-        Route::post('/stats', \App\Http\Controllers\Admin\StatsController::class);
+        Route::get('/stats', \App\Http\Controllers\Admin\StatsController::class);
     });
     // End of the /admin Route Group
 });
