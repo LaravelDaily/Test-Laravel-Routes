2761504824127620806cf9258f39166fe531c120
datasandwich <jarekettl98@gmail.com>
2023-07-05 09:18:02 +0100

    fifth commit

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::get('/user/{name}', [UserController::class, 'show']);
 // Put one code line here below
 
 Route::get('/about', function () {
-    return view('resources/views/pages/about.blade.php');
+    return view('pages/about.blade.php');
 })->name('about');
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
