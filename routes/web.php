90a15831e4a52693353821481998b900abcbb29e
HadyHawshar <110335391+HadyHawshar@users.noreply.github.com>
2023-07-18 17:40:57 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -75,7 +75,10 @@ Route::prefix('app')->group(function () {
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::get('/dashboard', DashboardController::class)->name("dashboard");
+        Route::namespace('Admin')->group(function () {
+            Route::resource('/dashboard', 'DashboardController');
+        });
+        
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
