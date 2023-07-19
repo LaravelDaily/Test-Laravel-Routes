9fe615a4c35b67a0837f418f01c7679e21ba6bf5
HadyHawshar <110335391+HadyHawshar@users.noreply.github.com>
2023-07-19 14:10:33 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -60,12 +60,12 @@ Route::prefix('app')->group(function () {
         // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
         // Assign the route name "dashboard"
         // Put one Route Group code line here below
-           Route::get('/app/dashboard' , DashboardController::class)->name('dashboard'); 
+           Route::get('/dashboard' , DashboardController::class)->name('dashboard'); 
 
         // Task 8: Manage tasks with URL /app/tasks/***.
         // Add ONE line to assign 7 resource routes to TaskController
         // Put one code line here below
-        Route::resource('/app/tasks' , TaskController::class);
+        Route::resource('/tasks' , TaskController::class);
 
         });  // End of the /app Route Group
 
@@ -82,11 +82,11 @@ Route::prefix('app')->group(function () {
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::resource('/admin/dashboard' , AdminDashboardController::class);
+        Route::resource('/dashboard' , AdminDashboardController::class);
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
-            Route::resource('/admin/stats' , StatsController::class);
+            Route::resource('/stats' , StatsController::class);
 
    }); // End of the /admin Route Group
 
