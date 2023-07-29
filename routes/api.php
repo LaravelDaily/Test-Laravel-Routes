4320d98a183d2c1c9d4cc2350ce1b5c542b118f4
pouria <109802957+pouria-azad@users.noreply.github.com>
2023-07-29 15:18:54 +0330

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | API Routes
