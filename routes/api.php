6e88bc069dbbaa4c78ac4a21f7eabf1b7ef811d3
batoul25 <92179135+batoul25@users.noreply.github.com>
2023-09-02 10:55:59 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
