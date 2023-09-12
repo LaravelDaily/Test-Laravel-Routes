0f36f0978e2371d82d54540a2c0f81b40feaa92e
Quan Ming <116425066+wahpiangle@users.noreply.github.com>
2023-09-12 18:09:01 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -70,32 +70,30 @@ Route::middleware('auth')->group(function () {
         // End of the /app Route Group
     });
 
-    Route::middleware('is_admin')->group(function () {
-        Route::prefix('admin')->group(function () {
-            // Task 9: /admin group within a group
-            // Add a group for routes with URL prefix "admin"
-            // Assign middleware called "is_admin" to them
-            // Put one Route Group code line here below
+    Route::group(['prefix' => 'admin', 'middleware' => 'is_admin'], function () {
+        // Task 9: /admin group within a group
+        // Add a group for routes with URL prefix "admin"
+        // Assign middleware called "is_admin" to them
+        // Put one Route Group code line here below
 
 
-            // Tasks inside that /admin group:
+        // Tasks inside that /admin group:
 
 
-            // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
-            // Put one code line here below
-            Route::get('dashboard', [AdminDashboardController::class]);
+        // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
+        // Put one code line here below
+        Route::get('dashboard', [AdminDashboardController::class]);
 
 
-            // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
-            // Put one code line here below
-            Route::get('stats', [StatsController::class]);
+        // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
+        // Put one code line here below
+        Route::get('stats', [StatsController::class]);
 
 
-            // End of the /admin Route Group
-        });
-
-        // End of the main Authenticated Route Group
+        // End of the /admin Route Group
     });
-    // One more task is in routes/api.php
+
+    // End of the main Authenticated Route Group
 });
+// One more task is in routes/api.php
 require __DIR__ . '/auth.php';
