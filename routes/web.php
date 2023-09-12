7573b518eaff5659001eabeff01d73ce9df8f459
Samsun Maarif <samsunmaarif62@gmail.com>
2023-09-12 08:15:18 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -26,16 +26,21 @@ Route::get('/', [HomeController::class, 'index']);
 // It doesn't use Route Model Binding, it expects $name as a parameter
 // Put one code line here below
 
-Route::get('/user/{name}','UserController@show');
+Route::get('/user/{name}', [UserController::class, 'show']);
 // Task 3: point the GET URL "/about" to the view
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-Route::view('/about','/pages/about.blade.php',['name'=>'about']);
+Route::get('/about', function () {
+    return view('pages.about');
+})->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
-Route::redirect('/log-in','login');
+Route::get('/log-in', function () {
+    return redirect('/login');
+});
+
 
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
