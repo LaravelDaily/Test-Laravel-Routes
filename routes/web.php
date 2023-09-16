c8d42c417c9b927532de0aef9a6c2fff69c12039
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2023-09-17 05:46:36 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-//use App\Http\Controllers\UserController;
-//use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
