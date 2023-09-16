b412b6e54efd4fb8b99e7dd767070691bcb4b71a
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2023-09-16 21:31:20 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -30,12 +30,12 @@ Route::get('/user/{name}',[UserController::class,'show']);
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-Route::view('/about','pages.about')->name('about');
+Route::view('/about','pages/about')->name('about');
 
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
-Route::redirect('/log-in','/login');
+Route::redirect('/log-in','/login',301);
 
 
 // Task 5: group the following route sentences below in Route::group()
