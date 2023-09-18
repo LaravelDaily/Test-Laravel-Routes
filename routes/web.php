dcdfaa060a0091fb07a7286f7ed193b400e6f2af
Thiago Sales <tsalescalabria@gmail.com>
2023-09-18 15:21:27 -0400

    More routes exercices

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,35 +29,40 @@ Route::get('/user/{name}', [UserController::class, 'show']);
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-
+Route::view('/about', 'pages.about')->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
-
+Route::redirect('/log-in', '/login');
 
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
 // Put one Route Group code line here below
+// Route::middleware(['auth'])->group(function () {
+Route::group(['middleware' => 'auth'], function () {
 
     // Tasks inside that Authenticated group:
 
     // Task 6: /app group within a group
     // Add another group for routes with prefix "app"
     // Put one Route Group code line here below
-
+    Route::prefix('app')->group(function () {
         // Tasks inside that /app group:
 
 
         // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
         // Assign the route name "dashboard"
         // Put one Route Group code line here below
+        Route::get('dashboard', DashboardController::class)->name('dashboard');
 
 
         // Task 8: Manage tasks with URL /app/tasks/***.
         // Add ONE line to assign 7 resource routes to TaskController
         // Put one code line here below
+        Route::get('tasks', DashboardController::class)->name('dashboard');
 
     // End of the /app Route Group
+    });
 
 
     // Task 9: /admin group within a group
@@ -79,6 +85,7 @@ Route::get('/user/{name}', [UserController::class, 'show']);
     // End of the /admin Route Group
 
 // End of the main Authenticated Route Group
+});
 
 // One more task is in routes/api.php
 
