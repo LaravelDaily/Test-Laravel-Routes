025551f8e6ca8cfcb14efcd646b353f2d49db804
Bimaekap <bimaeka2000@gmail.com>
2023-09-22 01:35:27 +0700

    update again

--- routes/web.php
+++ routes/web.php
@@ -76,7 +76,7 @@ Route::middleware(['auth'])->group(function () {
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
 
-        Route::get('/dashboard', App\Http\Controllers\AdminDashboardController::class);
+        Route::get('/dashboard', App\Http\Controllers\DashboardController::class);
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
         Route::get('/stats', App\Http\Controllers\Admin\StatsController::class);
