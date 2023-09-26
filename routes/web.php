0ed2fe02a2311200802ea917e6a78985dbc54e28
Thiago Sales <tsalescalabria@gmail.com>
2023-09-25 23:37:07 -0400

    All done

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\StatsController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -70,20 +72,22 @@ Route::group(['middleware' => 'auth'], function () {
     // Add a group for routes with URL prefix "admin"
     // Assign middleware called "is_admin" to them
     // Put one Route Group code line here below
-
+    Route::prefix('admin')->middleware('is_admin')->group(function () {
 
         // Tasks inside that /admin group:
 
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-
+        Route::get('dashboard', AdminDashboardController::class);
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
+        Route::get('stats', StatsController::class);
 
 
     // End of the /admin Route Group
+    });  
 
 // End of the main Authenticated Route Group
 });
