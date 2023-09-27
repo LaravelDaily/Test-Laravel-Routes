a1a29364408f88f0bb2417aad615336a8fec566e
dhanar98 <dhanasekarravi98@gmail.com>
2023-09-27 12:57:53 +0530

    Routes Test cases verified - Task 1 Completed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(15)->create();
+        \App\Models\Task::factory(15)->create();
+
     }
 }
