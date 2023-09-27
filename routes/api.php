6c25eea06665da0a96646bcf9a2ef89cfdd929c4
Asad Mansuri <asad.mansuri@matrixhive.com>
2023-09-27 17:01:18 +0530

    Test Laravel Routes - Completed

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,6 +23,10 @@ Route::group(['middleware' => 'auth:sanctum'], function() {
     // Task 12: Manage tasks with endpoint /api/v1/tasks/*****.
     // Keep in mind that prefix should be /api/v1.
     // Add ONE line to assign 5 resource routes to TaskController
+    Route::prefix('v1')->group(function() {
+        Route::apiResource('tasks', TaskController::class);
+
+    });
     // Put one code line here below
 
 });
