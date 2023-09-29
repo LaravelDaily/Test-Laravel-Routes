9116cc284c31ffab8f1964eaf1c36a31c7063b76
eXzacT <damjan.andelkovic1@gmail.com>
2023-09-29 13:15:39 +0200

    practice

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,6 @@ Route::group(['middleware' => 'auth:sanctum'], function() {
     // Task 12: Manage tasks with endpoint /api/v1/tasks/*****.
     // Keep in mind that prefix should be /api/v1.
     // Add ONE line to assign 5 resource routes to TaskController
-    // Put one code line here below
-
+    Route::resource('/api/v1',\App\Http\Controllers\Api\V1\TaskController::class)
+        ->except('create','edit');
 });
