37a64f303ffa14135aae482c2be4ea0322e5d9a4
Suzy William <suzy.william01@gmail.com>
2023-10-09 12:30:32 +0000

    Fix issue

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\V1\TaskController as ApiTaskController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\Api\V1\TaskController as ApiTaskController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
 use App\Http\Controllers\Admin\StatsController;
