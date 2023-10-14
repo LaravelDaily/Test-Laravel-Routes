19bd5222853c67d06ba37897863b9c51364e07ba
Davlat Ushurbakiev <dushurbakiev@gmail.com>
2023-10-14 23:21:43 +0600

    fix

--- routes/web.php
+++ routes/web.php
@@ -56,7 +56,7 @@ Route::group(['middleware' => 'auth'], function () {
         // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
         // Assign the route name "dashboard"
         // Put one Route Group code line here below
-        Route::singleton('dashboard', DashboardController::class);
+        Route::get('dashboard', DashboardController::class)->name('dashboard');
 
         // Task 8: Manage tasks with URL /app/tasks/***.
         // Add ONE line to assign 7 resource routes to TaskController
@@ -76,7 +76,7 @@ Route::group(['middleware' => 'auth'], function () {
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::singleton('dashboard', \App\Http\Controllers\Admin\DashboardController::class);
+        Route::get('dashboard', DashboardController::class)->name('dashboard');
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
         Route::singleton('stats', \App\Http\Controllers\Admin\StatsController::class);
