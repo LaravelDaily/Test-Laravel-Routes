9572d03b6f4643099b9aa282bacd540e9fa80e41
exe544 <a.lashko97@gmail.com>
2023-10-17 16:25:11 +0300

    solved by alashko

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,27 +18,44 @@ use Illuminate\Support\Facades\Route;
 
 // Task 1: point the main "/" URL to the HomeController method "index"
 // Put one code line here below
-
+Route::get('/', [HomeController::class, 'index']);
 
 // Task 2: point the GET URL "/user/[name]" to the UserController method "show"
 // It doesn't use Route Model Binding, it expects $name as a parameter
 // Put one code line here below
-
+Route::get('/user/{name}', [UserController::class, 'show']);
 
 // Task 3: point the GET URL "/about" to the view
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
+Route::get('/about', function () { return view('pages.about');})->name('about');
 
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
-
+Route::redirect('/login-in', '/login');
 
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
 // Put one Route Group code line here below
+Route::group(['middleware' => 'auth'], function () {
 
+    Route::prefix('/app')->group(function () {
+
+        Route::get('/dashboard', DashboardController::class)->name('dashboard');
+
+        Route::resource('tasks', \App\Http\Controllers\TaskController::class);
+
+    });
+    Route::group(['middleware' => 'is_admin', 'prefix' => 'admin'], function ()
+    {
+        Route::get('/dashboard', App\Http\Controllers\Admin\DashboardController::class);
+
+        Route::get('/stats', App\Http\Controllers\Admin\StatsController::class);
+
+    });
+});
     // Tasks inside that Authenticated group:
 
     // Task 6: /app group within a group
