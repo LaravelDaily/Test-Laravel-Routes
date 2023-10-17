35d1a57100c58ab556b09192cbf141bfb2b19106
Mahmoud Badr <80452075+Mahmoud-Badr-Abohussiba@users.noreply.github.com>
2023-10-17 11:10:00 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -26,11 +26,11 @@ Route::get('/user/{name}',[UserController::class,'show']);
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-Route::get('/about',function(){return view('pages.about')})->name('about');
+Route::get('/about',function(){return view('pages.about');})->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
-Route::get('log-in',function(){return redirect('login')});
+Route::get('log-in',function(){return redirect('login');});
 
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
