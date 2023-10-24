8ec4f2560e68ed0c15495934348d2d5b5f189c6f
spareparts23 <61853576+spareparts23@users.noreply.github.com>
2023-10-23 18:59:39 -0700

    add HomeController class to web.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,8 @@
         </style>
     </head>
     <body class="antialiased">
+
+    <h1>Welcome</h1>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
