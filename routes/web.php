b43090cc6e80e6f1dd81169d99bf8c456943d39a
M. Shokhibul Anwar <73224918+ShoAnn@users.noreply.github.com>
2023-10-26 12:06:06 +0700

    attempt 9

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\StatsController;
 use Illuminate\Support\Facades\Route;
