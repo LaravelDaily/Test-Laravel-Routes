262e3109dd25219110a6a251083038c06c6eb53a
M. Shokhibul Anwar <73224918+ShoAnn@users.noreply.github.com>
2023-10-27 10:22:46 +0700

    Attempt 24

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::get('/log-in', function () {
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
 // Put one Route Group code line here below
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
     // Tasks inside that Authenticated group:
 
     // Task 6: /app group within a group
