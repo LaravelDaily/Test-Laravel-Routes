1caa3089e1ca632a51a3a72025fdd46f648e1f71
M. Shokhibul Anwar <73224918+ShoAnn@users.noreply.github.com>
2023-10-27 15:38:45 +0700

    29

--- routes/web.php
+++ routes/web.php
@@ -73,7 +73,7 @@ Route::middleware(['auth'])->group(function () {
 // Add a group for routes with URL prefix "admin"
 // Assign middleware called "is_admin" to them
 // Put one Route Group code line here below
-Route::prefix('admin')->middleware(["is_admin"])->group(function () {
+Route::group(['prefix' => 'admin', 'middleware' => ['is_admin']], function () {
     // Tasks inside that /admin group:
     // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
     // Put one code line here below
