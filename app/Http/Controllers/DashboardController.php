b7ca83b6815f016c9027c05c74ae84d6a963d2e3
diagui <tounkarakan@gmail.com>
2023-10-29 17:58:15 +0300

    Laravel Routing Tests Passed Successfully

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function __invoke()
