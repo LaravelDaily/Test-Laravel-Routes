724086105dd831b0f9e4fba251df94aed5f8a035
OhmygoodR <109280933+OhmygoodR@users.noreply.github.com>
2023-10-29 10:51:52 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TaskController;
+use App\Http\Controllers\Api\V1\TaskController;
 
 /*
 |--------------------------------------------------------------------------
