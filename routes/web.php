b3d200155831703aafc37c048179d526c81afa01
Khanh Chung <khanh.d.chung@gmail.com>
2023-11-04 16:16:04 -0700

    task 3-7

--- routes/web.php
+++ routes/web.php
@@ -26,34 +26,34 @@ Route::get('/user/{name}', [\App\Http\Controllers\UserController::class, 'show']
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-
+Route::view('/about', 'pages.about')->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
-
+Route::redirect('/log-in', '/login');
 
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
 // Put one Route Group code line here below
-
+Route::middleware(['auth'])->group(function() {
     // Tasks inside that Authenticated group:
 
     // Task 6: /app group within a group
     // Add another group for routes with prefix "app"
     // Put one Route Group code line here below
-
+    Route::prefix('app')->group(function() {
         // Tasks inside that /app group:
-
 
         // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
         // Assign the route name "dashboard"
         // Put one Route Group code line here below
-
+        Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class])
+            ->name('dashboard');
 
         // Task 8: Manage tasks with URL /app/tasks/***.
         // Add ONE line to assign 7 resource routes to TaskController
         // Put one code line here below
-
+    });
     // End of the /app Route Group
 
 
@@ -75,7 +75,7 @@ Route::get('/user/{name}', [\App\Http\Controllers\UserController::class, 'show']
 
 
     // End of the /admin Route Group
-
+});
 // End of the main Authenticated Route Group
 
 // One more task is in routes/api.php
