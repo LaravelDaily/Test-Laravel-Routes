1ed42a7d5ac935d6c0aefefcd9f17d445cb01b53
DaveMolla <92756138+DaveMolla@users.noreply.github.com>
2023-11-07 10:10:30 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,15 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
-	@@ -15,69 +21,70 @@
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 // Task 1: point the main "/" URL to the HomeController method "index"
 // Put one code line here below
