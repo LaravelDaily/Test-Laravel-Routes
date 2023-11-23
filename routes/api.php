62bd841df054bd43fa6194adf127fdaa7c9a2ffc
John Loyd Lao <67764717+johnloydlao@users.noreply.github.com>
2023-11-23 20:10:05 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,6 @@ Route::group(['middleware' => 'auth:sanctum'], function() {
     // Task 12: Manage tasks with endpoint /api/v1/tasks/*****.
     // Keep in mind that prefix should be /api/v1.
     // Add ONE line to assign 5 resource routes to TaskController
-    // Put one code line here below
+    Route::resource('/api/v1/tasks', TaskController::class)->only(['index', 'create', 'store', 'show', 'edit']);
 
 });
