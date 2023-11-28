a2e26fd158508cb97463d2ee5e6fdc8fe3633608
King David <watcher24.inc@gmail.com>
2023-11-28 11:54:31 +0100

    Some trial and error

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::group(["middleware" => "auth"], function() {
         // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
         // Assign the route name "dashboard"
         // Put one Route Group code line here below
-        Route::get("/dashboard", [DashboardController::class]);
+        Route::get("/dashboard", DashboardController::class);
 
         // Task 8: Manage tasks with URL /app/tasks/***.
         // Add ONE line to assign 7 resource routes to TaskController
@@ -68,11 +68,11 @@ Route::group(["middleware" => "auth"], function() {
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::get('/dashboard', Admin/DashboardController::class);
+        Route::get('/dashboard', Admin\DashboardController::class);
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
-        Route::get('/stats', Admin/StatsController::class);
+        Route::get('/stats', Admin\StatsController::class);
 
     // End of the /admin Route Group
     });
