c2f95072f1957632dda0f58c019d599aa3e72337
Luiz Felipe <lepinholima@gmail.com>
2023-11-30 17:00:04 -0300

    fix: fix namespace definitions

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
-use App\Http\Controllers\StatsController;
+use App\Http\Controllers\Admin\StatsController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TaskController;
