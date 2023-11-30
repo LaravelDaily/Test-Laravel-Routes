b539ea9e50649e5c5f22d47612d5273252ab6722
Luiz Felipe <lepinholima@gmail.com>
2023-11-30 16:37:35 -0300

    fix: fix controller definition

--- routes/web.php
+++ routes/web.php
@@ -80,7 +80,7 @@ Route::middleware('auth')->group(function () {
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::get('/dashboard', AdminDashboardController::class);
+        Route::get('/dashboard', DashboardController::class);
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
