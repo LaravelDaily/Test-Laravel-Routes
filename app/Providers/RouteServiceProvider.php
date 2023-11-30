d8967df3271a334dd2ff822748f886527a4d775f
Luiz Felipe <lepinholima@gmail.com>
2023-11-30 16:20:28 -0300

    fix: fix api routes prefix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -39,7 +39,7 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::prefix('api')
-                ->middleware('api')
+                ->middleware('api/v1')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
