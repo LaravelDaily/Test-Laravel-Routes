b357a93114d8c364b5a9918c8e5983672f4ff0df
ahastarin <ahastarin@gmail.com>
2023-12-06 07:07:55 +0700

    new solution

--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::get('/user/{name}', [UserController::class, 'show']);
 // Put one code line here below
 Route::get('/about', function() {
     return view('pages.about');
-})
+});
 
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
