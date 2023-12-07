2a5c14a32047629b60fbfdfd50206958ef55e44a
King David <watcher24.inc@gmail.com>
2023-12-07 15:31:24 +0100

    Update web.php
    Imported Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    HomeController,
+    UserController,
+    DashboardController,
+    Admin\DashboardController as AdminDashboard,
+    Admin\StatsController as AdminStats
+};
 
 /*
 |--------------------------------------------------------------------------
@@ -68,11 +75,11 @@ Route::group(["middleware" => "auth"], function() {
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::get('/dashboard', Admin\DashboardController::class);
+        Route::get('/dashboard', AdminDashboard::class);
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
-        Route::get('/stats', Admin\StatsController::class);
+        Route::get('/stats', AdminStats::class);
 
     // End of the /admin Route Group
     });
