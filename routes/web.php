c0abf00bb6942ccb664eff122aaafae3dd8a5200
ssobii2 <subhanimran4@gmail.com>
2023-12-16 16:52:49 +0100

    Updated API and web routes

--- routes/web.php
+++ routes/web.php
@@ -73,7 +73,7 @@ Route::group(['middleware' => 'auth'], function () {
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::get('/dashboard', DashboardController::class);
+        Route::get('/dashboard', DashboardController::class)->name('admin.dashboard');
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
