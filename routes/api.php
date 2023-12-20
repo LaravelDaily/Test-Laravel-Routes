66527f19a5f4e7aa4fef4b11237454b8752c43b4
aexionn <corpsesbag@gmail.com>
2023-12-20 14:51:15 +0800

    Task Already Done

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,6 @@ Route::group(['middleware' => 'auth:sanctum'], function() {
     // Add ONE line to assign 5 resource routes to TaskController
     // Put one code line here below
     Route::prefix('v1')->group(function (){
-        Route::apiResources('/tasks', TaskController::class);
+        Route::apiResource('/tasks', TaskController::class);
     });
 });
