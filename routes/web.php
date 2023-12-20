451892d2d39e995b59e69015b329c2d3fb25a493
JonniNomo <143874944+JonniNomo@users.noreply.github.com>
2023-12-20 12:37:59 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::redirect('/log-in', '/login');
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
 // Put one Route Group code line here below
-Route::middleware('auth')->group(function() {
+Route::middleware('auth')->group(function(){
 
 })
     // Tasks inside that Authenticated group:
@@ -57,7 +57,7 @@ Route::middleware('auth')->group(function() {
     // Add another group for routes with prefix "app"
     // Put one Route Group code line here below
 
-    Route::prefix ('app')->group(function () {
+    Route::prefix ('app')->group(function(){
 
     
 
