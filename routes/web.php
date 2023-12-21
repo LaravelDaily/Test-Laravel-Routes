81764f1e56d106d2a239cb64205858a3dd04aa93
JonniNomo <143874944+JonniNomo@users.noreply.github.com>
2023-12-21 10:54:20 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -84,7 +84,7 @@ Route::redirect('/log-in', '/login');
     // Add a group for routes with URL prefix "admin"
     // Assign middleware called "is_admin" to them
     // Put one Route Group code line here below
-Route::prefix ('admin')->middleware::('is_admin')->group( function (){
+Route::prefix('admin')->middleware('is_admin')->group( function (){
 
 
 
