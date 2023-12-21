bf13fad007966d704fd774dd7aeb76978a120897
JonniNomo <143874944+JonniNomo@users.noreply.github.com>
2023-12-21 14:56:35 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\Admin\DashboardController as adminDashboardController;
 use App\Http\Controllers\Admin\StatsController as adminStatsController;
 
