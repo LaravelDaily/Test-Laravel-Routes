273da51d85ee51647127fc1d03b83e94532bec6a
hekruu1 <helinakruuk1@gmail.com>
2024-01-07 14:33:06 +0200

    Fixed middleware name in web.php.

--- routes/web.php
+++ routes/web.php
@@ -74,7 +74,7 @@ Route::middleware(['auth'])->group(function () {
     // Add a group for routes with URL prefix "admin"
     // Assign middleware called "is_admin" to them
     // Put one Route Group code line here below
-    Route::middleware('admin')->prefix('admin')->group(function () {
+    Route::middleware('is_admin')->prefix('admin')->group(function () {
         // Tasks inside that /admin group:
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
