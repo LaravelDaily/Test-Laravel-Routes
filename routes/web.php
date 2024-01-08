bbb80203195339546a761a9b7d82e414e8947ca3
Ebube Anthony Egolum <71565683+Tochard@users.noreply.github.com>
2024-01-08 14:35:15 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -76,11 +76,11 @@ Route::middleware(['auth']) -> group ( function() {
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::get('/dashboard', [DashboardController::class])
+        Route::get('/dashboard', [DashboardController::class]);
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
-             Route::get('/stats', [StatsController::class])
+             Route::get('/stats', [StatsController::class]);
 
 
     // End of the /admin Route Group
