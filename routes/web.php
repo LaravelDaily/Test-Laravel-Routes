41b8cf9c76d717f60556d3df3b00c2beb89531eb
Ebube Anthony Egolum <71565683+Tochard@users.noreply.github.com>
2024-01-08 14:33:41 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -56,13 +56,12 @@ Route::middleware(['auth']) -> group ( function() {
         // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
         // Assign the route name "dashboard"
         // Put one Route Group code line here below
-        Route::get('/dashboard', [DashboardController::class])->name('dashboard')
+        Route::get('/dashboard', [DashboardController::class])->name('dashboard');
 
         // Task 8: Manage tasks with URL /app/tasks/***.
         // Add ONE line to assign 7 resource routes to TaskController
         // Put one code line here below
-        // Route::resource('tasks', [TaskController::class]);
-        Route::resource("tasks", TaskController::class);
+        Route::resource('/tasks', [TaskController::class]);
     // End of the /app Route Group
     });
 
