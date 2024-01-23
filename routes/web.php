4e361dbdb3c96da0fb54532b95d238ee089fb524
Ahmad Essam <72272968+ahmadessam5@users.noreply.github.com>
2024-01-23 08:51:26 +0200

    All Done

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\TaskController:
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\Admin\DashboardController as AdminDashboard;
 use App\Http\Controllers\Admin\StatsController;
 
