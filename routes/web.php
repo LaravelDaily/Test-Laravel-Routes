44e1495f0a76f969f210f8b2b75a1cdf011040a3
Ahmad Essam <72272968+ahmadessam5@users.noreply.github.com>
2024-01-23 08:41:44 +0200

    complete all Route/web.app Tasks

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TaskController:
+use App\Http\Controllers\Admin\DashboardController as AdminDashboard;
+use App\Http\Controllers\Admin\StatsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,16 +33,29 @@ Route::get('/user/{name}', [UserController::class,'show']);
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-Route::view('/about','about')->name('about')
+Route::view('/about','about')->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
+Route::redirect('/log-in','/login');
 
 
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
 // Put one Route Group code line here below
-
+Route::middleware (['auth'])->group(function (){
+   Route::prefix('app')->group(function (){
+       Route::get('/dashboard',DashboardController::class)->name('dashboard');
+       Route::resource('tasks',TaskController::class);
+       
+   }); 
+    Route::prefix('admin')->group(function (){
+        Route::middleware('is_admin')->group(function(){
+            Route::get('/dashboard',AdminDashboard::class);
+            Route::get('/stats', StatsController::class);
+        });
+    });
+});
     // Tasks inside that Authenticated group:
 
     // Task 6: /app group within a group
