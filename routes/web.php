b29cff67a423685ae7d1bc61ffc6c88e0232781a
yuuukiDev <mohamedemadeldin34@gmail.com>
2024-02-06 18:49:29 +0200

    Updated Solutions

--- routes/web.php
+++ routes/web.php
@@ -68,11 +68,11 @@ Route::middleware('auth')->group(function(){
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-		Route::get('/admin/dashboard', DashboardController::class);
+		Route::get('/admin/dashboard', [DashboardController::class, 'index']);
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
-		Route::get('/admin/stats', StatsController::class);
+		Route::get('/admin/stats', [StatsController::class, 'index']);
 
     // End of the /admin Route Group
 	});
