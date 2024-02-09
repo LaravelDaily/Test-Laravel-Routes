3532dbe10e616c7b7b397e38509309ab2f5f08fc
haythamac <janssen.uy.dev@gmail.com>
2024-02-09 10:27:30 +0800

    Finished 12 tasks

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
+use App\Http\Controllers\Admin\StatsController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,21 +22,24 @@ use Illuminate\Support\Facades\Route;
 
 // Task 1: point the main "/" URL to the HomeController method "index"
 // Put one code line here below
+Route::get('/', [HomeController::class, 'index']);
 
 
 // Task 2: point the GET URL "/user/[name]" to the UserController method "show"
 // It doesn't use Route Model Binding, it expects $name as a parameter
 // Put one code line here below
-
+Route::get('/user/{name}', [UserController::class, 'show']);
 
 // Task 3: point the GET URL "/about" to the view
 // resources/views/pages/about.blade.php - without any controller
 // Also, assign the route name "about"
 // Put one code line here below
-
+Route::get('/about', function() { return url('pages.about'); })->name('about');
 
 // Task 4: redirect the GET URL "log-in" to a URL "login"
 // Put one code line here below
+Route::get('/log-in', function() { return url('login'); });
+
 
 
 // Task 5: group the following route sentences below in Route::group()
@@ -77,6 +87,24 @@ use Illuminate\Support\Facades\Route;
     // End of the /admin Route Group
 
 // End of the main Authenticated Route Group
+
+Route::group(['middleware' =>  'auth'], function() {
+    // App group
+    Route::group(['prefix' => '/app'], function() {
+        Route::resource('dashboard', DashboardController::class)->only('index');
+        Route::resource('tasks', TaskController::class);
+    });
+
+    // Admin group
+    Route::group([
+        'middleware' => 'is_admin',
+        'prefix' => '/admin'],
+        function() {
+            Route::resource('dashboard', AdminDashboardController::class);
+            Route::resource('stats', StatsController::class);
+
+    });
+});
 
 // One more task is in routes/api.php
 
