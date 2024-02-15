da98c8d8e390a569d3d728e0d3e0db522fc58da5
BeshoyAshraf10 <117385849+BeshoyAshraf10@users.noreply.github.com>
2024-02-15 19:17:37 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -61,7 +61,7 @@ Route::group(['middleware' => 'auth'], function (){
         // Task 7: point URL /app/dashboard to a "Single Action" DashboardController
         // Assign the route name "dashboard"
         // Put one Route Group code line here below
-        Route::get('/dashboard',DashboardController::class);
+        Route::get('/dashboard',DashboardController::class)->name('dashboard');
 
         // Task 8: Manage tasks with URL /app/tasks/***.
         // Add ONE line to assign 7 resource routes to TaskController
@@ -85,11 +85,11 @@ Route::group(['middleware' => 'auth'], function (){
         
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::get('/dashboard',DashboardController::class);
+        Route::get('/dashboard',Admin\DashboardController::class);
         
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
-        Route::get('/stats',StatsController::class);
+        Route::get('/stats',Admin\StatsController::class);
         
         
         // End of the /admin Route Group
