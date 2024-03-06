1c77b6a3a2d4d4e45b016edbebf4365216ba3709
Faros <114036707+a-faros@users.noreply.github.com>
2024-03-06 08:35:43 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\OrderController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
