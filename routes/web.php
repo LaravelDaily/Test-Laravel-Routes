e7b1f57dc8442b52e6277cfa175ceced43f26715
AljonPaulin <117250726+AljonPaulin@users.noreply.github.com>
2024-03-13 12:22:48 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -46,10 +46,10 @@ Route::get('/log-in', function () {
 // Task 5: group the following route sentences below in Route::group()
 // Assign middleware "auth"
 // Put one Route Group code line here below
-
-Route::middleware(['auth'])->group(function () {
-   //
+Route::group(['middleware' => 'auth'], function () {
+    // Routes that require authentication
 });
+
     // Tasks inside that Authenticated group:
 
     // Task 6: /app group within a group
