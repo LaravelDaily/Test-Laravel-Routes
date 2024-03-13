548b993f7d09add359b93058e95a457f2a8cae9a
AljonPaulin <117250726+AljonPaulin@users.noreply.github.com>
2024-03-13 12:19:40 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -47,20 +47,19 @@ Route::get('/log-in', function () {
 // Assign middleware "auth"
 // Put one Route Group code line here below
 
-Route::group(['middleware' => ['auth']], function () {
-//
+Route::middleware(['auth'])->group(function () {
+   //
 });
-
     // Tasks inside that Authenticated group:
 
     // Task 6: /app group within a group
     // Add another group for routes with prefix "app"
     // Put one Route Group code line here below
 Route::group("/app", function () {
-        Route::group(['prefix' => 'app'], function () {
-           //
-        });
-    });
+    Route::prefix('app')->group(function () {
+   //
+    });     
+});
         // Tasks inside that /app group:
 
 
