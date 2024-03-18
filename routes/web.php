1795eae7beb8dcd3d976b91044eb4e81ce8fe61a
Ranie <143940800+ranyesantos@users.noreply.github.com>
2024-03-18 17:52:43 -0300

    route features

--- routes/web.php
+++ routes/web.php
@@ -72,7 +72,7 @@ Route::middleware(['auth']) -> group(function(){
     // Add a group for routes with URL prefix "admin"
     // Assign middleware called "is_admin" to them
     // Put one Route Group code line here below
-    Route::middleware(['is_admin'])->prefix('/admin')->group(function(){
+    Route::middleware('is_admin')->prefix('admin')->group(function(){
 
         // Tasks inside that /admin group:
 
