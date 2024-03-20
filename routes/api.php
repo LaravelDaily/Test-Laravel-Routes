9d2fa81e743268072363317f2cc6c3bab6630110
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-03-20 18:15:54 +0100

    final 12/12

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,4 +25,5 @@ Route::group(['middleware' => 'auth:sanctum'], function() {
     // Add ONE line to assign 5 resource routes to TaskController
     // Put one code line here below
 
+    Route::resource('/api/v1/tasks', [TaskController::class]);
 });
