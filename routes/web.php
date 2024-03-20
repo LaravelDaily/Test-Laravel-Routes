ce80bb4863d758b407fdcb1398325d3c31529e69
Hammed Mujeeb <hamkamolujide@gmail.com>
2024-03-20 18:12:18 +0100

    11/12

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
+use App\Http\Controllers\Admin\StatsController;
 use App\Http\Controllers\Api\V1\TaskController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
@@ -69,20 +71,22 @@ Route::middleware('auth')->group(function() {
     // Add a group for routes with URL prefix "admin"
     // Assign middleware called "is_admin" to them
     // Put one Route Group code line here below
-    
-    
-    // Tasks inside that /admin group:
-    
-    
-    // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
-    // Put one code line here below
-    
-    
-    // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
-    // Put one code line here below
-    
-    
-    // End of the /admin Route Group
+    Route::middleware('is_admin')->group(function () {
+
+        
+        // Tasks inside that /admin group:
+        
+        
+        // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
+        // Put one code line here below
+        Route::get('/admin/dashboard', [AdminDashboardController::class]);
+        
+        // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
+        // Put one code line here below
+        Route::get('/admin/stats', [StatsController::class]);
+        
+        // End of the /admin Route Group
+    });
     
     // End of the main Authenticated Route Group
     
